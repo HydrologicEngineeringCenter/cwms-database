237696099a1696bd37226b172fb480342ea15a6d
krueger <a@b>
2006-12-14 11:03:36 -0800

    Revised av_loc so that elevation is returned in both SI and EN units.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 13889]

--- src/cwms/at_schema.sql
+++ src/cwms/at_schema.sql
@@ -2134,16 +2134,20 @@ CREATE OR REPLACE VIEW av_loc (location_code,
                                sub_location_id,
                                location_id,
                                location_type,
+                               unit_system,
                                elevation,
+                               unit_id,
+                               vertical_datum,
                                longitude,
                                latitude,
-                               description,
+                               horizontal_datum,
                                time_zone_name,
                                county_name,
                                state_initial,
-                               vertical_datum,
                                public_name,
-                               long_name
+                               long_name,
+                               description,
+                               active_flag
                               )
 AS
    SELECT apl.location_code, abl.base_location_code,
@@ -2152,23 +2156,30 @@ AS
              abl.base_location_id
           || SUBSTR ('-', 1, LENGTH (apl.sub_location_id))
           || apl.sub_location_id location_id,
-          apl.location_type, apl.elevation, apl.longitude, apl.latitude,
-          apl.description, ctz.time_zone_name, cc.county_name,
-          cs.state_initial, apl.vertical_datum, apl.public_name,
-          apl.long_name
+          apl.location_type, adu.unit_system,
+          apl.elevation * cuc.factor + cuc.offset elevation,
+          cuc.to_unit_id unit_id, apl.vertical_datum, apl.longitude,
+          apl.latitude, apl.horizontal_datum, ctz.time_zone_name,
+          cc.county_name, cs.state_initial, apl.public_name, apl.long_name,
+          apl.description, apl.active_flag
      FROM at_physical_location apl,
           at_base_location abl,
           cwms_county cc,
           cwms_office co,
           cwms_state cs,
-          cwms_time_zone ctz
-    WHERE (    (cc.county_code = NVL (apl.county_code, 0))
-           AND (cs.state_code = NVL (cc.state_code, 0))
-           AND (abl.db_office_code = co.office_code)
-           AND (ctz.time_zone_code = NVL (apl.time_zone_code, 0))
-           AND apl.base_location_code = abl.base_location_code
-		   AND apl.location_code <> 0
-          )
+          cwms_time_zone ctz,
+          at_display_units adu,
+          cwms_unit_conversion cuc
+    WHERE (cc.county_code = NVL (apl.county_code, 0))
+      AND (cs.state_code = NVL (cc.state_code, 0))
+      AND (abl.db_office_code = co.office_code)
+      AND (ctz.time_zone_code = NVL (apl.time_zone_code, 0))
+      AND apl.base_location_code = abl.base_location_code
+      AND apl.location_code != 0
+      AND adu.parameter_code =
+                         cwms_ts.get_parameter_code ('Elev', NULL, 'ALL', 'F')
+      AND cuc.from_unit_id = 'm'
+      AND cuc.to_unit_code = adu.display_unit_code
 /
 
 -------------------------
