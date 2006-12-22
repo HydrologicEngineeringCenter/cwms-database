5d3e652a4ddbed42129d90203dbcbfcbc057abd5
Mike Perryman <michael.d.perryman@usace.army.mil>
2006-12-22 15:33:24 -0800

    Reworked script order and added compilation of invalid objects at the end.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 13971]

--- src/buildCWMS_20_DB.sql
+++ src/buildCWMS_20_DB.sql
@@ -9,7 +9,6 @@ accept echo_state  char prompt 'Enter ON or OFF for echo       : '
 accept inst        char prompt 'Enter the database instance    : '
 accept sys_passwd  char prompt 'Enter the password for SYS     : '
 accept cwms_passwd char prompt 'Enter the password for CWMS_20 : '
-set echo &echo_state
 spool buildCWMS_20_DB.log
 --
 -- log on as sysdba
@@ -19,6 +18,7 @@ select sysdate from dual;
 set serveroutput on
 begin dbms_output.enable(20000); end;
 /
+set echo &echo_state
 --
 -- create the cwms_dev role and the cwms_20 user
 --
@@ -42,12 +42,6 @@ connect cwms_20/&cwms_passwd@&inst
 
 @@cwms/at_schema_tsv_dqu
 
-@@cwms/CWMS_LOC_PKG
-@@cwms/CWMS_LOC_PKG_BODY
-
-@@cwms/CWMS_CAT_PKG
-@@cwms/CWMS_CAT_PKG_BODY
-
 @@cwms/CWMS_TS_PKG
 @@cwms/CWMS_TS_PKG_BODY
 
@@ -55,6 +49,36 @@ connect cwms_20/&cwms_passwd@&inst
 @@cwms/CWMS_DSS_PKG_BODY
 
 @@cwms/at_schema_2
+
+@@cwms/CWMS_CAT_PKG
+@@cwms/CWMS_CAT_PKG_BODY
+
+@@cwms/CWMS_LOC_PKG
+@@cwms/CWMS_LOC_PKG_BODY
+
+--
+-- re-log on as sysdba and compile all invalid objects
+--
+set echo off
+connect sys/&sys_passwd@&inst as sysdba
+prompt Invalid objects...
+  select substr(object_name, 1, 31) "INVALID OBJECT", object_type 
+    from dba_objects 
+   where owner = 'CWMS_20' 
+     and status = 'INVALID'
+order by object_name, object_type asc;
+
+prompt Recompiling all invalid objects...
+exec utl_recomp.recomp_serial('CWMS_20');
+/
+
+prompt Remaining invalid objects...
+  select substr(object_name, 1, 31) "INVALID OBJECT", object_type 
+    from dba_objects 
+   where owner = 'CWMS_20' 
+     and status = 'INVALID'
+order by object_name, object_type asc;
+
 --
 -- all done
 --
