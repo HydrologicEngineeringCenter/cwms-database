ec8f3ab262a312593f773a924f38ed701edee8f6
Mike Perryman <michael.d.perryman@usace.army.mil>
2007-04-20 07:56:49 -0800

    Added AQ_USER and AQ_ADMINISTRATOR roles to CWMS_USER to allow users other than CWMS_20 to subscribe to the queues via JMS
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 15300]

--- src/cwms/User-Roles/cwms_user_roles.sql
+++ src/cwms/User-Roles/cwms_user_roles.sql
@@ -13,9 +13,13 @@ whenever sqlerror exit sql.sqlcode
 
 create role cwms_user not identified;
 grant create session to cwms_user;
+grant aq_user_role to cwms_user;
+grant aq_administrator_role to cwms_user;
+
 -- execute on packages granted later
 
 create role cwms_dev not identified;
 grant cwms_user to cwms_dev;
+
 -- select on views granted later
 
