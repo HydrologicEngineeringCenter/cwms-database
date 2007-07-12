75656b147156a5cf9036c13cfaa8e82c72d79c12
Mike Perryman <michael.d.perryman@usace.army.mil>
2007-07-12 12:59:15 -0800

    Added in timewindow, corrected minor naming errors
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 16049]

--- test/scripts/validateDataexchange.py
+++ test/scripts/validateDataexchange.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, re, sys
 import java, javax, org
 
 errorCount        = 0
@@ -6,6 +6,18 @@ warningCount      = 0
 totalErrorCount   = 0
 totalWarningCount = 0
 instanceCount     = 0
+
+iso_time_pattern = re.compile("-?\d{4}-\d{2}-\d{2}T\d{2}:\d{2}(:\d{2}([.]\d+)?)?([-+]\d{2}:\d{2}|Z)?")
+
+#-------------------------------------------------------------------------------------------#
+# this XPath operation strips all '.' and '/' characters and white space from the node text #
+#-------------------------------------------------------------------------------------------#
+_strip = "normalize-space(translate(text(), '/. ', ''))" 
+#---------------------------------------------------------------------------------------------#
+# this XPath operation selects all ts-mapping nodes that have "stripped" text ending with "%" #
+#---------------------------------------------------------------------------------------------#
+_filter = "ts-mapping-set/ts-mapping/*[contains(substring(%s, string-length(%s)), '%%')]/.." % (_strip, _strip)
+print 'Forecast mapping filter = "%s"' % _filter
 
 class errorHandler(org.xml.sax.ErrorHandler) :
 
@@ -107,6 +119,9 @@ def validateXML(xmlSpec) :
 		xchg_set_id = xpath.evaluate("@id", xchg_set, STRING)
 		realtime_sourceid = xpath.evaluate("@realtime-source-id", xchg_set, STRING)
 
+		mapping_count = xpath.evaluate("count(ts-mapping-set/*)", xchg_set, NUMBER)
+		print "\t%s (%d mappings)" % (xchg_set_id, mapping_count)
+		
 		if is_cwms :
 			#-------------------------------------------------------------------------------#
 			# verify that there is one DSS and one Oracle data store for CWMS exchange sets #
@@ -121,6 +136,47 @@ def validateXML(xmlSpec) :
 					'dataexchange-set "%s" must use one oracle datastore and one dssfilemanager datastore.' % \
 					xchg_set_id, None))
 
+		#---------------------------------------------------------------------#					
+		# verify that forecast mappings have matching parameterized versions  #
+		# and parameterized forecast time windows                             #
+		#                                                                     #
+		# the XPath gets a little hairy (c'mon XPath 2.0!), but operates much #
+		# faster than iterating through every node in the script code...      #
+		#---------------------------------------------------------------------#
+		mappings = xpath.evaluate(_filter, xchg_set, NODESET)
+		for j in range(mappings.getLength()) :
+			mapping = mappings.item(j)
+			if xpath.evaluate("name(*[1])", mapping, STRING) == 'dss-timeseries' :
+				version1 = xpath.evaluate("*[1]", mapping, STRING).strip().split("/")[6].strip()
+			else :
+				version1 = xpath.evaluate("*[1]", mapping, STRING).strip().split(".")[5].strip()
+			if xpath.evaluate("name(*[2])", mapping, STRING) == 'dss-timeseries' :
+				version2 = xpath.evaluate("*[2]", mapping, STRING).strip().split("/")[6].strip()
+			else :
+				version2 = xpath.evaluate("*[2]", mapping, STRING).strip().split(".")[5].strip()
+			if version1 == "%" * len(version1) or version2 == "%" * len(version2) :
+				if version1 != version2 :
+					errorHandlerInst.error(org.xml.sax.SAXParseException(
+						'dataexchange-set "%s" has forecast mapping with mis-mathced version place-holders' % \
+						xchg_set_id, None))
+						
+			tw_node = xpath.evaluate("../../timewindow", mapping, NODE)
+			if not tw_node :
+				errorHandlerInst.error(org.xml.sax.SAXParseException(
+					'dataexchange-set "%s" has forecast mapping without a forecast time window.' % \
+					xchg_set_id, None))
+			starttime = xpath.evaluate("normalize-space(starttime)", tw_node, STRING)
+			endtime = xpath.evaluate("normalize-space(endtime)", tw_node, STRING)
+			if iso_time_pattern.match(starttime) :
+				errorHandlerInst.error(org.xml.sax.SAXParseException(
+					'dataexchange-set "%s" has forecast time window with explicit start time.' % \
+					xchg_set_id, None))
+			if iso_time_pattern.match(endtime) :
+				errorHandlerInst.error(org.xml.sax.SAXParseException(
+					'dataexchange-set "%s" has forecast time window with explicit end time.' % \
+					xchg_set_id, None))
+			
+				
 		#-------------------------------------------------------------------------------------------#
 		# verify that no time series is used for realtime import and export from the same datastore #
 		#-------------------------------------------------------------------------------------------#
