dab6c5431d8801253efdfbb7ffa58cf566ce371f
krueger <a@b>
2007-08-30 14:40:26 -0800

    added a get_user_office_id wraper for cwms_util.user_office_id
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 16381]

--- src/cwms/cwms_sec_pkg.sql
+++ src/cwms/cwms_sec_pkg.sql
@@ -1,4 +1,4 @@
-/* Formatted on 2007/06/27 14:24 (Formatter Plus v4.8.8) */
+/* Formatted on 2007/08/30 15:37 (Formatter Plus v4.8.8) */
 CREATE OR REPLACE PACKAGE cwms_sec
 AS
    PROCEDURE get_user_priv_groups (
@@ -6,5 +6,8 @@ AS
       p_username       IN       VARCHAR2 DEFAULT NULL,
       p_db_office_id   IN       VARCHAR2 DEFAULT NULL
    );
+
+   FUNCTION get_user_office_id
+      RETURN VARCHAR2;
 END cwms_sec;
 /
--- src/cwms/cwms_sec_pkg_body.sql
+++ src/cwms/cwms_sec_pkg_body.sql
@@ -1,25 +1,24 @@
-/* Formatted on 2007/06/28 10:49 (Formatter Plus v4.8.8) */
+/* Formatted on 2007/08/30 15:38 (Formatter Plus v4.8.8) */
 CREATE OR REPLACE PACKAGE BODY cwms_sec
 AS
-
 --------------------------------------------------------------------------------
 --The get_user_priv_groups procedure returns a refcursor of:
 
---        db_office_id   varchar2(16)
+   --        db_office_id   varchar2(16)
 --        username       varchar2(31)
 --        user_group_id  varchar2(32)
 
---The user_group_id's returned are ONLY the CWMS system level user groups, not
---locally created user groups, i.e., "CWMS DBA Users", "Super CWMS Users", 
---"CWMS PD User", "Data Exchange Mgr", "Data Acquisition Mgr", "TS ID Creator", 
+   --The user_group_id's returned are ONLY the CWMS system level user groups, not
+--locally created user groups, i.e., "CWMS DBA Users", "Super CWMS Users",
+--"CWMS PD User", "Data Exchange Mgr", "Data Acquisition Mgr", "TS ID Creator",
 --"VT Mgr", and "All Users".
 
---If p_username is null, then the system will use the username for the current 
---session. If p_db_office_id is null, then the default db_office_id of the 
---username will be used. Once can also pass in "ALL" for the p_db_office_id, 
---which will return the priv groups for all db_office_id's associated with 
+   --If p_username is null, then the system will use the username for the current
+--session. If p_db_office_id is null, then the default db_office_id of the
+--username will be used. Once can also pass in "ALL" for the p_db_office_id,
+--which will return the priv groups for all db_office_id's associated with
 --the username.
---   
+--
    PROCEDURE get_user_priv_groups (
       p_priv_groups    OUT      sys_refcursor,
       p_username       IN       VARCHAR2 DEFAULT NULL,
@@ -51,6 +50,13 @@ AS
                AND a.db_office_code =
                                  cwms_util.get_db_office_code (l_db_office_id);
       END IF;
+   END;
+
+   FUNCTION get_user_office_id
+      RETURN VARCHAR2
+   IS
+   BEGIN
+      RETURN cwms_util.user_office_id;
    END;
 END cwms_sec;
 /
