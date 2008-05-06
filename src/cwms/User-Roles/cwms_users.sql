c17f4aab4a0816ac0528d65267803548d7acce25
Mike Perryman <michael.d.perryman@usace.army.mil>
2008-05-06 11:16:09 -0800

    changed method of detecting whether running in 10 or 11 database
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 19234]

--- src/cwms/User-Roles/cwms_users.sql
+++ src/cwms/User-Roles/cwms_users.sql
@@ -99,8 +99,6 @@ grant select on  sys.v_$statname to cwms_20;
 grant select on  sys.v_$timer to cwms_20;
 grant update any table to cwms_20;
 
-declare
-   compatible_version v$parameter.value%type;
 begin
    --
    -- grant queue privileges
@@ -118,28 +116,23 @@ begin
       grantee      => 'cwms_20',
       admin_option => false);
    
-   select value
-     into compatible_version
-     from v$parameter
-    where name = 'compatible';
-
-   if compatible_version > '11.0.0.0.0' then
-      --
-      -- grant network address resolve privileges (new in Oracle 11)
-      --
-      begin
-         dbms_network_acl_admin.create_acl(
-            acl         => 'resolve.xml',
-            description => 'resolve acl', 
-            principal   => 'CWMS_20', 
-            is_grant    => true, 
-            privilege   => 'resolve');
-         dbms_network_acl_admin.assign_acl(
-            acl         => 'resolve.xml', 
-            host        => '*');
-      exception
-         when others then
-            if sqlcode = 31003 then
+   --
+   -- grant network address resolve privileges (new in Oracle 11)
+   --
+   begin
+      dbms_network_acl_admin.create_acl(
+         acl         => 'resolve.xml',
+         description => 'resolve acl', 
+         principal   => 'CWMS_20', 
+         is_grant    => true, 
+         privilege   => 'resolve');
+      dbms_network_acl_admin.assign_acl(
+         acl         => 'resolve.xml', 
+         host        => '*');
+   exception
+      when others then
+         case sqlcode
+            when 31003 then
                --
                -- acl already exists, re-assign to newly-created CWMS_20 user
                --
@@ -148,14 +141,13 @@ begin
                   principal   => 'CWMS_20', 
                   is_grant    => true, 
                   privilege   => 'resolve');
-            end if;
-      end;
-   else
-      --
-      -- Not applicable for Oracle 10 database
-      --
-      dbms_output.put_line('Skipping Oracle 11 network acl setup.');
-   end if;
+            when 6550 then
+               --
+               -- must be running pre-11 database
+               --
+               null;
+         end case;
+   end;
 end;
 /
 
