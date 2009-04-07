f3dd0d7077bc796d20d30c94cac6e3fc35afe9c5
Mike Perryman <michael.d.perryman@usace.army.mil>
2009-04-07 14:25:37 -0800

    Added 2009 table and restructured table creation code.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 22872]

--- src/cwms/at_schema_tsv.sql
+++ src/cwms/at_schema_tsv.sql
@@ -2,314 +2,57 @@
 -- AT_CWMS_TSV_**** table
 -- 
 
-CREATE TABLE AT_TSV
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING
-/
-CREATE TABLE AT_TSV_2008
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2008_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING
-/
-CREATE TABLE AT_TSV_2007
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2007_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING
-/
-CREATE TABLE AT_TSV_2006
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2006_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING
-/
-CREATE TABLE AT_TSV_2005
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2005_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING
-/
-CREATE TABLE AT_TSV_2004
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2004_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING;
-
-
-CREATE TABLE AT_TSV_2003
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2003_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING;
-
-CREATE TABLE AT_TSV_2002
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_2002_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING;
-
-CREATE TABLE AT_TSV_ARCHIVAL
-(
-  TS_CODE          NUMBER(10)                   NOT NULL,
-  DATE_TIME        DATE                         NOT NULL,
-  VERSION_DATE     DATE                         NOT NULL,
-  DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
-  VALUE            BINARY_DOUBLE,
-  QUALITY_CODE     NUMBER, 
-  CONSTRAINT AT_TSV_ARCHIVAL_PK
- PRIMARY KEY
- (TS_CODE, DATE_TIME, VERSION_DATE)
-)
-ORGANIZATION INDEX
-LOGGING
-TABLESPACE CWMS_20_TSV
-PCTFREE    10
-INITRANS   2
-MAXTRANS   255
-STORAGE    (
-            INITIAL          64K
-            MINEXTENTS       1
-            MAXEXTENTS       2147483645
-            PCTINCREASE      0
-            BUFFER_POOL      DEFAULT
-           )
-COMPRESS 2
-NOPARALLEL
-MONITORING;
-
------------------------------
--- AT_TSV_#### constraints --
---
-
-ALTER TABLE AT_TSV ADD (
-  CONSTRAINT AT_TSV_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/ 
-ALTER TABLE AT_TSV_2008 ADD (
-  CONSTRAINT AT_TSV_2008_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
-ALTER TABLE AT_TSV_2007 ADD (
-  CONSTRAINT AT_TSV_2007_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
-ALTER TABLE AT_TSV_2006 ADD (
-  CONSTRAINT AT_TSV_2006_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
-ALTER TABLE AT_TSV_2005 ADD (
-  CONSTRAINT AT_TSV_2005_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/ 
-ALTER TABLE AT_TSV_2004 ADD (
-  CONSTRAINT AT_TSV_2004_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
-ALTER TABLE AT_TSV_2003 ADD (
-  CONSTRAINT AT_TSV_2003_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
- ALTER TABLE AT_TSV_2002 ADD (
-  CONSTRAINT AT_TSV_2002_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
-/
- ALTER TABLE AT_TSV_ARCHIVAL ADD (
-  CONSTRAINT AT_TSV_ARCHIVAL_FK 
- FOREIGN KEY (TS_CODE) 
- REFERENCES AT_CWMS_TS_SPEC (TS_CODE))
+DECLARE
+   TYPE name_tab_t IS TABLE OF VARCHAR2(16);
+   l_names name_tab_t := NEW name_tab_t();
+   l_template_1 VARCHAR2(1024) := '
+      CREATE TABLE AT_TSV$name
+      (
+        TS_CODE          NUMBER(10)                   NOT NULL,
+        DATE_TIME        DATE                         NOT NULL,
+        VERSION_DATE     DATE                         NOT NULL,
+        DATA_ENTRY_DATE  TIMESTAMP(6)                 NOT NULL,
+        VALUE            BINARY_DOUBLE,
+        QUALITY_CODE     NUMBER, 
+        CONSTRAINT AT_TSV$name_PK
+       PRIMARY KEY
+       (TS_CODE, DATE_TIME, VERSION_DATE)
+      )
+      ORGANIZATION INDEX
+      LOGGING
+      TABLESPACE CWMS_20_TSV
+      PCTFREE    10
+      INITRANS   2
+      MAXTRANS   255
+      STORAGE    (
+                  INITIAL          64K
+                  MINEXTENTS       1
+                  MAXEXTENTS       2147483645
+                  PCTINCREASE      0
+                  BUFFER_POOL      DEFAULT
+                 )
+      COMPRESS 2
+      NOPARALLEL
+      MONITORING';
+      
+   l_template_2 VARCHAR2(256) := '
+    ALTER TABLE AT_TSV$name ADD (
+     CONSTRAINT AT_TSV$name_FK 
+    FOREIGN KEY (TS_CODE) 
+    REFERENCES AT_CWMS_TS_SPEC (TS_CODE))';
+   
+BEGIN
+   l_names.extend(2);
+   l_names(1) := '';
+   l_names(2) := '_ARCHIVAL';
+   FOR i IN  2002..2009 LOOP
+      l_names.extend;
+      l_names(l_names.count) := '_' || i;
+   END LOOP;
+   FOR i IN 1..l_names.last LOOP
+      execute IMMEDIATE REPLACE(l_template_1, '$name', l_names(i));
+      execute IMMEDIATE REPLACE(l_template_2, '$name', l_names(i));
+      COMMIT;
+   END LOOP;
+END;
 /
