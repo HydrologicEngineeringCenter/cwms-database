c455d6e781ec9ede169b18814117247d6aa80fc3
krueger <a@b>
2009-05-05 05:37:32 -0800

    Add the av_unit view.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 23065]

--- src/cwms/at_schema_av.sql
+++ src/cwms/at_schema_av.sql
@@ -22,7 +22,8 @@ DECLARE
                      'av_screening_criteria',
                      'av_screening_dur_mag',
                      'av_screening_id',
-                     'av_shef_decode_spec'
+                     'av_shef_decode_spec',
+                     'av_unit'
                     );
 BEGIN
    FOR i IN view_names.FIRST .. view_names.LAST
@@ -519,4 +520,35 @@ AS
           ts_code, db_office_code, base_location_code, location_code,
           parameter_code
      FROM mv_cwms_ts_id
+/
+--------------------------------------------------------------------------------
+CREATE OR REPLACE FORCE VIEW av_unit
+(
+    unit_system,
+    unit_id,
+    abstract_param_id,
+    long_name,
+    description,
+    db_office_id,
+    unit_code,
+    abstract_param_code,
+    db_office_code
+)
+AS
+    SELECT    a.unit_system, a.unit_id, b.abstract_param_id, a.long_name,
+                a.description, 'CWMS' db_office_id, a.unit_code,
+                a.abstract_param_code, 53 db_office_code
+      FROM    cwms_unit a, cwms_abstract_parameter b
+     WHERE    b.abstract_param_code = a.abstract_param_code
+    UNION
+    SELECT    b.unit_system, a.alias_id unit_id, c.abstract_param_id,
+                b.long_name, b.description, d.office_id db_office_id, a.unit_code,
+                b.abstract_param_code, a.db_office_code
+      FROM    at_unit_alias a,
+                cwms_unit b,
+                cwms_abstract_parameter c,
+                cwms_office d
+     WHERE         b.unit_code = a.unit_code
+                AND b.abstract_param_code = c.abstract_param_code
+                AND a.db_office_code = d.office_code
 /
