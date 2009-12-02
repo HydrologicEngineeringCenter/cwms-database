e5d5f6e03fe4642d7b078b6b1e1594810494bd91
Mike Perryman <michael.d.perryman@usace.army.mil>
2009-12-02 08:33:43 -0800

    Restrored AV_LOC_ALIAS to previous structure and added AV_LOC_GROUP_ASSGN
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 25179]

--- src/cwms/at_schema_av.sql
+++ src/cwms/at_schema_av.sql
@@ -266,7 +266,6 @@ CREATE OR REPLACE VIEW av_loc_alias (category_id,
                                      base_location_id,
                                      sub_location_id,
                                      location_id,
-                                     attribute,
                                      alias_id
                                     )
 AS
@@ -276,8 +275,39 @@ AS
              abl.base_location_id
           || SUBSTR ('-', 1, LENGTH (atpl.sub_location_id))
           || atpl.sub_location_id location_id,
-          atlga.loc_attribute,
           atlga.loc_alias_id
+     FROM at_physical_location atpl,
+          at_base_location abl,
+          at_loc_group_assignment atlga,
+          at_loc_group atlg,
+          at_loc_category atlc,
+          cwms_office co
+    WHERE atlga.location_code = atpl.location_code
+      AND atpl.base_location_code = abl.base_location_code
+      AND atlga.loc_group_code = atlg.loc_group_code
+      AND atlg.loc_category_code = atlc.loc_category_code
+      AND abl.db_office_code = co.office_code
+/
+--------------------------------------------------------------------------------
+CREATE OR REPLACE VIEW av_loc_grp_assgn (category_id,
+                                         group_id,
+                                         location_code,
+                                         db_office_id,
+                                         base_location_id,
+                                         sub_location_id,
+                                         location_id,
+                                         alias_id,
+                                         attribute
+                                        )
+AS
+   SELECT atlc.loc_category_id, atlg.loc_group_id, atlga.location_code,
+          co.office_id db_office_id, abl.base_location_id,
+          atpl.sub_location_id,
+             abl.base_location_id
+          || SUBSTR ('-', 1, LENGTH (atpl.sub_location_id))
+          || atpl.sub_location_id location_id,
+          atlga.loc_alias_id,
+          atlga.loc_attribute
      FROM at_physical_location atpl,
           at_base_location abl,
           at_loc_group_assignment atlga,
