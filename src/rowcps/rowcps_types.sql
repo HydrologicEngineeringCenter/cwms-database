fc5bb7eba03dcbf8ccfe5cb72611bc82b0046086
Pete Morris <psmorris@rmanet.com>
2010-04-27 16:02:10 -0800

    Removed the delete call, rowcps_types is dependent on and run by cwms_types so execute that script instead. Updated the rowcps types to database rev 7.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 27287]

--- src/rowcps/rowcps_types.sql
+++ src/rowcps/rowcps_types.sql
@@ -1,104 +1,38 @@
 WHENEVER sqlerror exit sql.sqlcode
 SET define on
 @@../cwms/defines.sql
-
 SET serveroutput on
 
-------------------------------
--- drop types if they exist --
-------------------------------
+--location objects are defined in cwms_types.
+--location_ref_t and location_obj_t.
 
-DECLARE
-   not_defined        EXCEPTION;
-   has_dependencies   EXCEPTION;
-   PRAGMA EXCEPTION_INIT (not_defined, -4043);
-   PRAGMA EXCEPTION_INIT (has_dependencies, -2303);
 
-   TYPE id_array_t IS TABLE OF VARCHAR2 (32);
-
-   dropped_count      PLS_INTEGER;
-   defined_count      PLS_INTEGER;
-   total_count        PLS_INTEGER := 0;
-   pass_count         PLS_INTEGER := 0;
-   type_names         id_array_t
-      := id_array_t (
-'project_obj_t',
-'lookup_type_obj_t',
-'embank_protection_type_obj_t',
-'embank_structure_type_obj_t',
-'embankment_obj_t',
-'PHYSICAL_TRANSFER_TYPE_OBJ_T',
-'water_user_obj_t',
-'wat_user_contract_obj_t',
-'wat_usr_contract_acct_obj_t'
-                    );
-BEGIN
-   defined_count := type_names.COUNT;
-
-   LOOP
-      pass_count := pass_count + 1;
-      DBMS_OUTPUT.put_line ('Pass ' || pass_count);
-      dropped_count := 0;
-      DBMS_OUTPUT.put_line ('');
-
-      FOR i IN type_names.FIRST .. type_names.LAST
-      LOOP
-         IF LENGTH (type_names (i)) > 0
-         THEN
-            BEGIN
-               EXECUTE IMMEDIATE 'drop type ' || type_names (i);
-
-               DBMS_OUTPUT.put_line ('   Dropped type ' || type_names (i));
-               dropped_count := dropped_count + 1;
-               total_count := total_count + 1;
-               type_names (i) := '';
-            EXCEPTION
-               WHEN not_defined
-               THEN
-                  IF pass_count = 1
-                  THEN
-                     defined_count := defined_count - 1;
-                  END IF;
-               WHEN has_dependencies
-               THEN
-                  NULL;
-            END;
-         END IF;
-      END LOOP;
-
-      EXIT WHEN dropped_count = 0;
-   END LOOP;
-
-   DBMS_OUTPUT.put_line ('');
-
-   IF total_count != defined_count
-   THEN
-      DBMS_OUTPUT.put ('*** WARNING: Only ');
-   END IF;
-
-   DBMS_OUTPUT.put_line (   ''
-                         || total_count
-                         || ' out of '
-                         || defined_count
-                         || ' types dropped'
-                        );
-END;
+CREATE OR REPLACE TYPE lookup_type_obj_t AS OBJECT
+(
+    display_value	varchar2(25 byte),  --The value to display for this lookup record
+    tooltip	varchar2(255 byte),     --The tooltip or meaning of this lookup record
+    active	varchar2(1 byte)	--Whether this lookup record entry is currently active
+);
 /
 show errors
 
+CREATE OR REPLACE TYPE document_obj_t AS OBJECT(
+    document_id	VARCHAR2(64 BYTE)	-- The unique identifier for the individual document, user provided
+);
+/
+show errors
 
---location objectss.
-
-
+CREATE OR REPLACE TYPE document_tab_t IS TABLE OF document_obj_t;
+/
+show errors
 
 --project object.
 CREATE OR REPLACE TYPE project_obj_t AS OBJECT (
     --locations
-    --the office id for the base location
-    db_office_id 			VARCHAR2(16),
 
-    -- the location associated with this project,
+    --the location associated with this project,
     --an instance of the location type.
+    --has the db office id for this project.
     project_location 		location_obj_t,
 
     --The location code where the water is pumped back to
@@ -153,71 +87,86 @@ CREATE OR REPLACE TYPE project_obj_t AS OBJECT (
 /
 show errors
 
-create or replace type lookup_type_obj_t as object
-(
-    display_value	varchar2(25 byte),  --The value to display for this lookup record
-    tooltip	varchar2(255 byte),     --The tooltip or meaning of this lookup record
-    active	varchar2(1 byte)	--Whether this lookup record entry is currently active
-);
-/
-show errors
+
 
 CREATE OR REPLACE TYPE embankment_obj_t AS OBJECT
 ( 
     project_location_ref	location_ref_t,          --The project this embankment is a child of
     embankment_location location_obj_t,          --The location for this embankment 
     structure_type	lookup_type_obj_t,           --The lookup code for the type of the embankment structure
-    structure_length	number,           --The overall length of the embankment structure
+    structure_length	BINARY_DOUBLE,           --The overall length of the embankment structure
     upstream_prot_type	lookup_type_obj_t,           --The upstream protection type code for the embankment structure
-    upstream_sideslope	number,           --The upstream side slope of the embankment structure
+    upstream_sideslope	BINARY_DOUBLE,           --The upstream side slope of the embankment structure
     downstream_prot_type	lookup_type_obj_t,   --The downstream protection type codefor the embankment structure
-    downstream_sideslope	number,           --The downstream side slope of the embankment structure
-    height_max	number,                   --The maximum height of the embankment structure
-    top_width	number                    --The width at the top of the embankment structure
+    downstream_sideslope	BINARY_DOUBLE,           --The downstream side slope of the embankment structure
+    height_max	BINARY_DOUBLE,                   --The maximum height of the embankment structure
+    top_width	BINARY_DOUBLE                    --The width at the top of the embankment structure
 );
 /
 show errors
 
-create or replace type water_user_obj_t as object
+
+CREATE OR REPLACE TYPE water_user_obj_t AS OBJECT
 ( 
     project_location_ref    location_ref_t,      --The project that this user is pertaining to.
-    ENTITY_NAME	VARCHAR2(64 BYTE),      --The entity name associated with this user
-    WATER_RIGHT	VARCHAR2(255 BYTE)      --The water right of this user
+    entity_name	VARCHAR2(64 BYTE),      --The entity name associated with this user
+    water_right	VARCHAR2(255 BYTE)      --The water right of this user
 );
 / 
+show errors
+
+CREATE OR REPLACE TYPE water_user_contract_ref_t AS OBJECT
+(
+    water_user	water_user_obj_t,--The water user this record pertains to.  See table AT_WATER_USER.
+    contract_name	VARCHAR2(64 BYTE)--The identification name for the contract for this water user contract
+);
+/
 show errors
 
 create or replace type wat_user_contract_obj_t as object
-( 
+(
+    water_user_contract_ref water_user_contract_ref_t,
+   
+    contracted_storage	BINARY_DOUBLE,--The contracted storage amount for this water user contract
+
+    contract_documents	VARCHAR2(64 BYTE),--The documents for the contract
+    water_supply_contract_type lookup_type_obj_t, -- The type of water supply contract. FK'ed to a LU table.
+
+    ws_contract_effective_date	DATE,--The start date of the contract for this water user contract
+    ws_contract_expiration_date	DATE,--The expiration date for the contract of this water user contract
+
+    initial_use_allocation	BINARY_DOUBLE,--The initial contracted allocation for this water user contract
+    future_use_allocation	BINARY_DOUBLE,--The future contracted allocation for this water user contract
+
+    future_use_percent_activated	BINARY_DOUBLE,--The percent allocated future use for this water user contract
+    total_alloc_percent_activated	BINARY_DOUBLE,--The percentage of total allocation for this water user contract
     
-    CONTRACT_WATER_USER	water_user_obj_t,--The water user this record pertains to.  See table AT_WATER_USER.
-    CONTRACT_NAME	VARCHAR2(64 BYTE),--The identification name for the contract for this water user contract
-    SUPPLY_LOCATION	location_ref_t,--The location where the supply of water for this contract will come from.  See AT_PHYSICAL_LOCATION
-    CONTRACTED_STORAGE	NUMBER,--The contracted storage amount for this water user contract
-    CONTRACT_DOCUMENTS	VARCHAR2(64 BYTE),--The documents for the contract
-    WS_CONTRACT_EFFECTIVE_DATE	DATE,--The start date of the contract for this water user contract
-    WS_CONTRACT_EXPIRATION_DATE	DATE,--The expiration date for the contract of this water user contract
-    INITIAL_USE_ALLOCATION	NUMBER,--The initial contracted allocation for this water user contract
-    FUTURE_USE_ALLOCATION	NUMBER,--The future contracted allocation for this water user contract
-    FUTURE_USE_PERCENT_ACTIVATED	NUMBER,--The percent allocated future use for this water user contract
-    TOTAL_ALLOC_PERCENT_ACTIVATED	NUMBER,--The percentage of total allocation for this water user contract
-    WITHDRAW_LOCATION	location_ref_t --The location where this user contract withdraws or pumps out their water
+    withdraw_location	location_obj_t, --The code for the AT_PHYSICAL_LOCATION record which is the location where this water with be withdrawn from the permanent pool
+    supply_location	    location_obj_t --The AT_PHYSICAL_LOCATION record which is the location where this water will be obtained below the dam or within the outlet works
 );
 / 
 show errors
 
+
 create or replace type wat_usr_contract_acct_obj_t as object
 (
-    WUSR_CONTRACT_ACCT_CONTR	wat_user_contract_obj_t,--The contract for this water movement. SEE AT_WATER_USER_CONTRACT.
-    PHYSICAL_TRANSFER_TYPE	lookup_type_obj_t,--The type of transfer for this water movement.  See AT_LU_PHYSICAL_TRANSFER_TYPE_CODE.
-    ACCOUNTING_CREDIT_DEBIT	VARCHAR2(6 BYTE),--Whether this water movement is a credit or a debit to the contract
-    ACCOUNTING_VOLUME	NUMBER,--The volume associated with the water movement
-    TRANSFER_START_DATETIME	DATE,--The date this water movement began
-    TRANSFER_END_DATETIME	DATE,--the date this water movement stopped
-    ACCOUNTING_REMARKS	VARCHAR2(255 BYTE) --Any comments regarding this water accounting movement
+    water_user_contract_ref water_user_contract_ref_t,--The contract for this water movement. SEE AT_WATER_USER_CONTRACT.
+    physical_transfer_type	lookup_type_obj_t,--The type of transfer for this water movement.  See AT_LU_PHYSICAL_TRANSFER_TYPE_CODE.
+    accounting_credit_debit	VARCHAR2(6 BYTE),--Whether this water movement is a credit or a debit to the contract
+    accounting_volume	BINARY_DOUBLE,--The volume associated with the water movement
+    transfer_start_datetime	DATE,--The date this water movement began
+    transfer_end_datetime	DATE,--the date this water movement stopped
+    accounting_remarks	VARCHAR2(255 BYTE) --Any comments regarding this water accounting movement
 );
 / 
 show errors
+
+CREATE OR REPLACE TYPE wat_usr_contract_acct_tab_t is table of wat_usr_contract_acct_obj_t;
+/
+show errors
+
+
+
 
 
 
