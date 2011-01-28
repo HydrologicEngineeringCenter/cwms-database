f47ef4e24e9c4549481d42bc75a4b8171eaf1cd3
Mike Perryman <michael.d.perryman@usace.army.mil>
2011-01-28 13:05:35 -0800

    Updated rounding method to match ADAPS manual
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 32983]

--- src/cwms/cwms_rounding_pkg.sql
+++ src/cwms/cwms_rounding_pkg.sql
@@ -1,97 +1,106 @@
 create or replace package cwms_rounding
 as
 --------------------------------------------------------------------------------
+-- This package contains routines that implement the USGS rounding operation
+-- described in Section 3.5 of the ADAPS manual at:
+--    http://pubs.usgs.gov/of/2003/ofr03123/
+--
+-- The rounding information can be found at:
+--    http://pubs.usgs.gov/of/2003/ofr03123/3.overview.pdf#rounding
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
 -- round_nn_f
 function round_nn_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return number;   
 
 --------------------------------------------------------------------------------
 -- round_nd_f
 function round_nd_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return binary_double;   
 
 --------------------------------------------------------------------------------
 -- round_nt_f
 function round_nt_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return varchar2;   
 
 --------------------------------------------------------------------------------
 -- round_dd_f
 function round_dd_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return binary_double;   
 
 --------------------------------------------------------------------------------
 -- round_dn_f
 function round_dn_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return number;   
 
 --------------------------------------------------------------------------------
 -- round_dt_f
 function round_dt_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return varchar2;   
 
 --------------------------------------------------------------------------------
 -- round_td_f
 function round_td_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return binary_double;   
 
 --------------------------------------------------------------------------------
 -- round_tn_f
 function round_tn_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return number;   
 
 --------------------------------------------------------------------------------
 -- round_tt_f
 function round_tt_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return varchar2;   
 
 --------------------------------------------------------------------------------
 -- round_n_tab
 procedure round_n_tab(
-   p_values        in out nocopy number_tab_t, -- input as table of numbers
-   p_rounding_spec in            varchar2);    -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy number_tab_t,
+   p_rounding_spec in            varchar2);
 
 --------------------------------------------------------------------------------
 -- round_d_tab
 procedure round_d_tab(
-   p_values        in out nocopy double_tab_t, -- input as table of binary_doubles
-   p_rounding_spec in            varchar2);    -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy double_tab_t,
+   p_rounding_spec in            varchar2);
 
 --------------------------------------------------------------------------------
 -- round_t_tab
 procedure round_t_tab(
-   p_values        in out nocopy str_tab_t, -- input as table of text
-   p_rounding_spec in            varchar2); -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy str_tab_t,
+   p_rounding_spec in            varchar2);
 
 --------------------------------------------------------------------------------
 -- round_tsv_array
 procedure round_tsv_array(
-   p_values        in out nocopy tsv_array, -- input as tsv_array
-   p_rounding_spec in            varchar2); -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy tsv_array,
+   p_rounding_spec in            varchar2);
 
 --------------------------------------------------------------------------------
 -- round_ztsv_array
 procedure round_ztsv_array(
-   p_values        in out nocopy ztsv_array, -- input as ztsv_array
-   p_rounding_spec in            varchar2);  -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy ztsv_array,
+   p_rounding_spec in            varchar2);
    
 end cwms_rounding;
 /
--- src/cwms/cwms_rounding_pkg_body.sql
+++ src/cwms/cwms_rounding_pkg_body.sql
@@ -29,37 +29,41 @@ end validate_rounding_spec;
 --------------------------------------------------------------------------------
 -- round_nn_f
 function round_nn_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return number   
 is
+   l_value      number;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_value is null then
       return null;
    elsif p_value = 0 then
       return 0;      
-   else
+   else                
       validate_rounding_spec(p_rounding_spec);
-      l_magnitude  := trunc(log(10, abs(p_value)));
-      l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+      l_value      := l_value;
+      l_magnitude  := trunc(log(10, abs(l_value)));
+      l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
       l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
       if l_sig_digits = 0 then
-         return p_value; -- no rounding
+         l_value := 0;
       end if;
-      l_dec_places := l_sig_digits - l_magnitude - 1;
-      return round(p_value, l_dec_places);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
+      l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+      return round(l_value, l_dec_places);
    end if;
 end round_nn_f;
 
 --------------------------------------------------------------------------------
 -- round_nd_f
 function round_nd_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return binary_double   
 is
 begin
@@ -69,8 +73,8 @@ end round_nd_f;
 --------------------------------------------------------------------------------
 -- round_nt_f
 function round_nt_f(
-   p_value         in  number,   -- input as number
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  number,
+   p_rounding_spec in  varchar2)
 return varchar2   
 is
 begin
@@ -80,37 +84,41 @@ end round_nt_f;
 --------------------------------------------------------------------------------
 -- round_dd_f
 function round_dd_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return binary_double   
 is
+   l_value      binary_double;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_value is null then
       return null;
    elsif p_value = 0 then
       return 0;      
-   else
+   else                
       validate_rounding_spec(p_rounding_spec);
-      l_magnitude  := trunc(log(10, abs(p_value)));
-      l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+      l_value      := l_value;
+      l_magnitude  := trunc(log(10, abs(l_value)));
+      l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
       l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
       if l_sig_digits = 0 then
-         return p_value; -- no rounding
+         l_value := 0;
       end if;
-      l_dec_places := l_sig_digits - l_magnitude - 1;
-      return round(p_value, l_dec_places);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
+      l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+      return round(l_value, l_dec_places);
    end if;
 end round_dd_f;
 
 --------------------------------------------------------------------------------
 -- round_dn_f
 function round_dn_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return number   
 is
 begin
@@ -120,8 +128,8 @@ end round_dn_f;
 --------------------------------------------------------------------------------
 -- round_dt_f
 function round_dt_f(
-   p_value         in  binary_double, -- input as binary_double
-   p_rounding_spec in  varchar2)      -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  binary_double,
+   p_rounding_spec in  varchar2)
 return varchar2   
 is
 begin
@@ -131,8 +139,8 @@ end round_dt_f;
 --------------------------------------------------------------------------------
 -- round_td_f
 function round_td_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return binary_double
 is
 begin
@@ -142,8 +150,8 @@ end round_td_f;
 --------------------------------------------------------------------------------
 -- round_tn_f
 function round_tn_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return number
 is
 begin
@@ -153,8 +161,8 @@ end round_tn_f;
 --------------------------------------------------------------------------------
 -- round_tt_f
 function round_tt_f(
-   p_value         in  varchar2, -- input as text
-   p_rounding_spec in  varchar2) -- string of significant digits for powers of 10 in range -3..6
+   p_value         in  varchar2,
+   p_rounding_spec in  varchar2)
 return varchar2
 is
 begin
@@ -164,24 +172,29 @@ end round_tt_f;
 --------------------------------------------------------------------------------
 -- round_n_tab
 procedure round_n_tab(
-   p_values        in out nocopy number_tab_t, -- input as table of numbers
-   p_rounding_spec in            varchar2)     -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy number_tab_t,
+   p_rounding_spec in            varchar2)
 is
+   l_value      number;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_values is not null then
       validate_rounding_spec(p_rounding_spec);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
       for i in 1..p_values.count loop
-         l_magnitude  := trunc(log(10, abs(p_values(i))));
-         l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+         l_value      := p_values(i);
+         l_magnitude  := trunc(log(10, abs(l_value)));
+         l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
          l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
-         if l_sig_digits != 0 then
-            l_dec_places := l_sig_digits - l_magnitude - 1;
-            p_values(i) := round(p_values(i), l_dec_places);
+         if l_sig_digits = 0 then
+            l_value := 0;
          end if;
+         l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+         p_values(i)  := round(l_value, l_dec_places);
       end loop;
    end if;
 end round_n_tab;
@@ -189,24 +202,29 @@ end round_n_tab;
 --------------------------------------------------------------------------------
 -- round_d_tab
 procedure round_d_tab(
-   p_values        in out nocopy double_tab_t, -- input as table of binary_doubles
-   p_rounding_spec in            varchar2)     -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy double_tab_t,
+   p_rounding_spec in            varchar2)
 is
+   l_value      binary_double;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_values is not null then
       validate_rounding_spec(p_rounding_spec);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
       for i in 1..p_values.count loop
-         l_magnitude  := trunc(log(10, abs(p_values(i))));
-         l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+         l_value      := p_values(i);
+         l_magnitude  := trunc(log(10, abs(l_value)));
+         l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
          l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
-         if l_sig_digits != 0 then
-            l_dec_places := l_sig_digits - l_magnitude - 1;
-            p_values(i) := round(p_values(i), l_dec_places);
+         if l_sig_digits = 0 then
+            l_value := 0;
          end if;
+         l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+         p_values(i)  := round(l_value, l_dec_places);
       end loop;
    end if;
 end round_d_tab;
@@ -214,26 +232,29 @@ end round_d_tab;
 --------------------------------------------------------------------------------
 -- round_t_tab
 procedure round_t_tab(
-   p_values        in out nocopy str_tab_t, -- input as table of text
-   p_rounding_spec in            varchar2)  -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy str_tab_t,
+   p_rounding_spec in            varchar2)
 is
    l_value      number;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_values is not null then
       validate_rounding_spec(p_rounding_spec);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
       for i in 1..p_values.count loop
          l_value      := to_number(p_values(i));
          l_magnitude  := trunc(log(10, abs(l_value)));
-         l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+         l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
          l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
-         if l_sig_digits != 0 then
-            l_dec_places := l_sig_digits - l_magnitude - 1;
-            p_values(i) := to_char(round(l_value, l_dec_places));
+         if l_sig_digits = 0 then
+            l_value := 0;
          end if;
+         l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+         p_values(i)  := round(l_value, l_dec_places);
       end loop;
    end if;
 end round_t_tab;
@@ -241,24 +262,29 @@ end round_t_tab;
 --------------------------------------------------------------------------------
 -- round_tsv_array
 procedure round_tsv_array(
-   p_values        in out nocopy tsv_array, -- input as tsv_array
-   p_rounding_spec in            varchar2)  -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy tsv_array,
+   p_rounding_spec in            varchar2)
 is
+   l_value      binary_double;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_values is not null then
       validate_rounding_spec(p_rounding_spec);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
       for i in 1..p_values.count loop
-         l_magnitude  := trunc(log(10, abs(p_values(i).value)));
-         l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+         l_value      := p_values(i).value;
+         l_magnitude  := trunc(log(10, abs(l_value)));
+         l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
          l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
-         if l_sig_digits != 0 then
-            l_dec_places := l_sig_digits - l_magnitude - 1;
-            p_values(i).value := round(p_values(i).value, l_dec_places);
+         if l_sig_digits = 0 then
+            l_value := 0;
          end if;
+         l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+         p_values(i).value := round(l_value, l_dec_places);
       end loop;
    end if;
 end round_tsv_array;
@@ -266,24 +292,29 @@ end round_tsv_array;
 --------------------------------------------------------------------------------
 -- round_ztsv_array
 procedure round_ztsv_array(
-   p_values        in out nocopy ztsv_array, -- input as ztsv_array
-   p_rounding_spec in            varchar2)   -- string of significant digits for powers of 10 in range -3..6
+   p_values        in out nocopy ztsv_array,
+   p_rounding_spec in            varchar2)
 is
+   l_value      binary_double;
    l_magnitude  binary_integer; -- integer power of 10
    l_spec_pos   binary_integer; -- 1-based position in rounding spec
    l_sig_digits binary_integer; -- significant digits based on rounding spec
    l_dec_places binary_integer; -- decimal places to round input number to
+   l_max_places binary_integer; -- maximum number of decimal places
 begin
    if p_values is not null then
       validate_rounding_spec(p_rounding_spec);
+      l_max_places := to_number(substr(p_rounding_spec, 10));
       for i in 1..p_values.count loop
-         l_magnitude  := trunc(log(10, abs(p_values(i).value)));
-         l_spec_pos   := least(6, greatest(-3, l_magnitude)) + 4;
+         l_value      := p_values(i).value;
+         l_magnitude  := trunc(log(10, abs(l_value)));
+         l_spec_pos   := least(5, greatest(-3, l_magnitude)) + 4;
          l_sig_digits := to_number(substr(p_rounding_spec, l_spec_pos, 1));
-         if l_sig_digits != 0 then
-            l_dec_places := l_sig_digits - l_magnitude - 1;
-            p_values(i).value := round(p_values(i).value, l_dec_places);
+         if l_sig_digits = 0 then
+            l_value := 0;
          end if;
+         l_dec_places := least(l_sig_digits - l_magnitude - 1, l_max_places);
+         p_values(i).value := round(l_value, l_dec_places);
       end loop;
    end if;
 end round_ztsv_array;
