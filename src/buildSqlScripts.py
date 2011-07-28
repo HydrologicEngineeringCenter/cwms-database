207b4345d728ecb903b257981c3261d90f35c973
Mike Perryman <michael.d.perryman@usace.army.mil>
2011-07-28 13:01:02 -0800

    Added CWMS_MSG_ID table
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 36200]

--- src/buildSqlScripts.py
+++ src/buildSqlScripts.py
@@ -131,6 +131,7 @@ tableInfo = [
     {"ID" : "gageType",           "TABLE" : "CWMS_GAGE_TYPE",             "SCHEMA" : "CWMS", "USERACCESS" : True},
     {"ID" : "nation",             "TABLE" : "CWMS_NATION",                "SCHEMA" : "CWMS", "USERACCESS" : True},
     {"ID" : "streamType",         "TABLE" : "CWMS_STREAM_TYPE",           "SCHEMA" : "CWMS", "USERACCESS" : True},
+    {"ID" : "msgId",              "TABLE" : "CWMS_MSG_ID",                "SCHEMA" : "CWMS", "USERACCESS" : True},
 ]
 
 tables = []
@@ -8749,6 +8750,34 @@ for v1, v2, v3, v4, v5, v6, v7 in streamTypes :
     streamTypeLoadTemplate += "INSERT INTO @TABLE VALUES ('%s', '%s', '%s', '%s', '%s', '%s', '%s');\n" % (v1, v2, v3, v4, v5, v6, v7)
 streamTypeLoadTemplate += "COMMIT;\n"
 
+sys.stderr.write("Building msgIdCreationTemplate\n")
+msgIdCreationTemplate = \
+'''
+-- ## TABLE ###############################################
+-- ## @TABLE
+-- ##
+CREATE TABLE @TABLE
+(
+   LAST_MILLIS INTEGER,
+   LAST_SEQ    INTEGER
+)
+tablespace CWMS_20DATA
+/
+
+---------------------------
+-- @TABLE comments --
+--
+COMMENT ON TABLE  @TABLE             IS 'Contains data to generate message ids based on millisecond and sequence within millisecond';
+COMMENT ON COLUMN @TABLE.LAST_MILLIS IS 'Millisecond of last message id generated';
+COMMENT ON COLUMN @TABLE.LAST_SEQ    IS 'Sequence number within millisecond for last message id generated';
+
+COMMIT;
+'''
+sys.stderr.write("Building msgIdLoadTemplate\n")
+msgIdLoadTemplate = '''
+   INSERT INTO @TABLE VALUES (0, 0);
+   COMMIT;
+'''
 
 
 #==
