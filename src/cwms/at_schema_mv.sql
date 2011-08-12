4304389ee66f516c7021901d9d51a92dd204a3a1
Mike Perryman <michael.d.perryman@usace.army.mil>
2011-08-12 14:36:54 -0800

    Moved materialized views for data quality stuff from at_schema_av to at_schema_mv
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 36485]

--- src/cwms/at_schema_mv.sql
+++ src/cwms/at_schema_mv.sql
@@ -5,6 +5,16 @@ DECLARE
 	TYPE id_array_t IS TABLE OF VARCHAR2 (32);
 
 	table_names   id_array_t := id_array_t ('at_cwms_ts_id');
+   view_names    id_array_t := id_array_t('mv_data_quality',
+                                       	'mv_data_q_changed',
+                                       	'mv_data_q_protection',
+                                       	'mv_data_q_range',
+                                       	'mv_data_q_repl_cause',
+                                       	'mv_data_q_repl_method',
+                                       	'mv_data_q_screened',
+                                       	'mv_data_q_test_failed',
+                                       	'mv_data_q_validity');
+
 BEGIN
 	FOR i IN table_names.FIRST .. table_names.LAST
 	LOOP
@@ -18,7 +28,26 @@ BEGIN
 				NULL;
 		END;
 	END LOOP;
+   for i in 1..view_names.count loop
+      begin
+         dbms_output.put('Dropping materialized view '||view_names(i)||'...');
+         execute immediate 'drop materialized view '||view_names(i);
+         dbms_output.put_line('done');
+      exception
+         when others then dbms_output.put_line(sqlerrm);
+      end;
+   end loop;
 END;
 /
 
 @@cwms/tables/at_cwms_ts_id.sql
+
+@@cwms/views/mv_data_q_changed.sql
+@@cwms/views/mv_data_q_protection.sql
+@@cwms/views/mv_data_q_range.sql
+@@cwms/views/mv_data_q_repl_cause.sql
+@@cwms/views/mv_data_q_repl_method.sql
+@@cwms/views/mv_data_q_screened.sql
+@@cwms/views/mv_data_q_test_failed.sql
+@@cwms/views/mv_data_q_validity.sql
+@@cwms/views/mv_data_quality.sql
