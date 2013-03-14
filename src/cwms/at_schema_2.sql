45335de94908b65c4dc7364894d0dd46f0fc6b4d
Mike Perryman <michael.d.perryman@usace.army.mil>
2013-03-14 14:36:40 -0800

    Moved index AT_LOC_LVL_INDICATOR_U1 to after API loading to allow rounding of attribute value to 9 places in index
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 48473]

--- src/cwms/at_schema_2.sql
+++ src/cwms/at_schema_2.sql
@@ -1,3 +1,13 @@
+create index at_loc_lvl_indicator_u1 on at_loc_lvl_indicator (
+   location_code,
+   specified_level_code,
+   parameter_code,
+   duration_code,
+   level_indicator_id,
+   cwms_rounding.round_nn_f(attr_value, '9999999999'),
+   attr_parameter_code,
+   ref_specified_level_code,
+   ref_attr_value);
 ---------------------------------------------------------
 @@cwms/views/av_loc.sql
 @@cwms/views/av_log_message.sql
@@ -7,4 +17,4 @@
 @@cwms/views/av_loc_lvl_ts_map.sql
 @@cwms/views/av_loc_lvl_cur_max_ind.sql
 ---------------------------------------------------------
-COMMIT;
+COMMIT;                                                                                                                                                     
