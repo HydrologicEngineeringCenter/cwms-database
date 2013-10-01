5930573f99a1a6fe204bbe3e3e96c6b2b180faef
Rami Gasim <rami@rmanet.com>
2013-10-01 12:14:55 -0800

    1) added arguments support to pass parameters (e.g. hecjavadevRelDir, versionDir, p4WorkspaceName, etc...) from the build system. It still works the same without specifying any arguments 2) removed check for parameter code > 0 3) added "#position=100" line
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 52405]

--- build_db_parameters_def.py
+++ build_db_parameters_def.py
@@ -1,5 +1,7 @@
 import os, subprocess, sys
 moduleName = "buildSqlScripts"
+argv = sys.argv
+hecjavadevDir = "../../../../hecjavadev"
 #-----------------------------------------#
 # get program directory from command line #
 #-----------------------------------------#
@@ -11,7 +13,17 @@ print("%s : Starting up" % progName)
 #-------------------------------------------------#
 progDir    = os.path.normpath(progDir)
 buildVer   = os.path.split(progDir)[1]
-outputDir  = os.path.normpath(os.path.join(progDir, "../../../../hecjavadev", buildVer, "apps/config/db/definitions"))
+
+if (len(argv) == 4): 
+	hecjavadevDir = argv[1]
+	buildVer = argv[2]
+	p4WorkspaceName = argv[3]
+elif (len(argv) > 1):
+	sys.stderr.write("Usage: python build_db_parameters_def.py <hecjavadev_dir> <build_version> <p4_workspace_name> \n")
+	sys.stderr.write("Ex:    python buildSqlScripts.py build_db_parameters_def.py ../hecjavadev dev COMPUTE01-wcdba-v1.0\n")
+	sys.exit(-1)
+
+outputDir  = os.path.normpath(os.path.join(progDir, hecjavadevDir, buildVer, "apps/config/db/definitions"))
 outputFile = os.path.join(outputDir, "db_parameters_units.def")
 #----------------------------------------------------------------------#
 # import the buildSqlScripts script used to generate the static schema #
@@ -29,19 +41,23 @@ if exists :
 	f = open(outputFile, "r")
 	oldFileData = f.read()
 	f.close()
-	cmd = "p4 edit %s" % outputFile
+	if (len(argv) == 4):
+		cmd = "p4 -c %s edit %s" % (p4WorkspaceName, outputFile)
+	else:
+		cmd = "p4 edit %s" % outputFile
 	print("%s : %s" % (progName, cmd))
 	subprocess.call(cmd)
 #------------------------------------------------------------------------------------#
 # use the parameters variable from the imported script to generate the resource file #
 #------------------------------------------------------------------------------------#
-params = sorted([[p[2],p[5],p[6]] for p in buildSqlScripts.parameters if p[0] > 0])
+params = sorted([[p[2],p[5],p[6]] for p in buildSqlScripts.parameters])
 maxIdLen = max([len(param[0]) for param in params])
 maxSiLen = max([len(param[1]) for param in params])
 format = "%%-%ds : %%-%ds : %%s\n" % (maxIdLen, maxSiLen)
 print("%s : Writing resource file %s" % (progName, outputFile))
 f = open(outputFile, "w")
-f.write("\n=PARAMETER/UNIT\n\n")
+f.write("\n=PARAMETER/UNIT\n")
+f.write("#position=100\n\n")
 for param_id, si_unit, en_unit in params :
 	f.write(format % (param_id, si_unit, en_unit))
 f.close()
@@ -52,16 +68,25 @@ f.close()
 # add the resource file if it didn't exist #
 #------------------------------------------#
 if not exists :
-	cmd = "p4 add %s" % outputFile
+	if (len(argv) == 4):
+		cmd = "p4 -c %s add %s" % (p4WorkspaceName, outputFile)
+	else:
+		cmd = "p4 add %s" % outputFile
 	print("%s : %s" % (progName, cmd))
-	subprocess.call(cmd)
+	subprocess.call(cmd)	
 #------------------------------------#
 # submit or revert the resource file #
 #------------------------------------#
 if newFileData == oldFileData :
-	cmd = "p4 revert %s" % outputFile
-else :	
-	cmd = "p4 submit -d auto_update %s" % outputFile
+	if (len(argv) == 4):
+		cmd = "p4 -c %s revert %s" % (p4WorkspaceName, outputFile)
+	else:
+		cmd = "p4 revert %s" % outputFile
+else :
+	if (len(argv) == 4):
+		cmd = "p4 -c %s submit -d auto_update %s" % (p4WorkspaceName, outputFile)
+	else:
+		cmd = "p4 submit -d auto_update %s" % outputFile
 print("%s : %s" % (progName, cmd))
 subprocess.call(cmd)
 
