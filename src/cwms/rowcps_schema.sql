58bfe247561bdd3627d94e1dba2a35ed68daa915
Mike Perryman <michael.d.perryman@usace.army.mil>
2014-04-01 12:37:37 -0800

    Added constraints to force pump_in, pump_out, and pump_out_below to be separate locations.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 55835]

--- src/cwms/rowcps_schema.sql
+++ src/cwms/rowcps_schema.sql
@@ -2387,6 +2387,14 @@ NOPARALLEL
 /
 
 ALTER TABLE at_water_user_contract ADD (
+  CONSTRAINT at_water_user_contract_ck1
+  CHECK (nvl(pump_out_location_code, -1) not in (nvl(pump_out_below_location_code, -2), nvl(pump_in_location_code, -3)))
+/      
+ALTER TABLE at_water_user_contract ADD (
+  CONSTRAINT at_water_user_contract_ck2
+  CHECK (nvl(pump_out_below_location_code, -2) != nvl(pump_in_location_code, -3))
+/
+ALTER TABLE at_water_user_contract ADD (
   CONSTRAINT at_water_user_contract_fk2
  FOREIGN KEY (water_user_code)
  REFERENCES at_water_user (water_user_code))
