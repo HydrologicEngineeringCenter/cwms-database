7d11d4f9ae7766a6d3e43adadde3449c15de2d29
Mike Perryman <michael.d.perryman@usace.army.mil>
2014-04-25 10:45:38 -0800

    Bugfix in basins view
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 56468]

--- src/cwms/views/av_basin.sql
+++ src/cwms/views/av_basin.sql
@@ -18,7 +18,7 @@ insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_BASIN', null,
  * @field parent_basin_code          The unique numeric code that identifies the parent basin in the database
  */
 ');
-create or replace force view cwms_20.av_basin(
+create or replace force view av_basin(
    office_id,
    basin_id,
    primary_stream_id,
@@ -31,7 +31,6 @@ create or replace force view cwms_20.av_basin(
    primary_stream_code,
    parent_basin_code)
 as
-   -- primary stream and parent basin
    select o.office_id,
           bl1.base_location_id || substr('-', 1, length(pl1.sub_location_id)) || pl1.sub_location_id as basin_id,
           bl2.base_location_id || substr('-', 1, length(pl2.sub_location_id)) || pl2.sub_location_id as primary_stream_id,
@@ -55,86 +54,10 @@ as
     where pl1.location_code = b.basin_location_code
       and bl1.base_location_code = pl1.base_location_code
       and o.office_code = bl1.db_office_code
-      and pl2.location_code = b.primary_stream_code
-      and bl2.base_location_code = pl2.base_location_code
-      and pl3.location_code = b.parent_basin_code
-      and bl3.base_location_code = pl3.base_location_code
+      and pl2.location_code(+) = b.primary_stream_code
+      and bl2.base_location_code(+) = pl2.base_location_code
+      and pl3.location_code(+) = b.parent_basin_code
+      and bl3.base_location_code(+) = pl3.base_location_code
       and cu.from_unit_id = cwms_util.get_default_units('Area', 'SI')
-   union all
-   -- primary stream and no parent basin
-   select o.office_id,
-          bl1.base_location_id || substr('-', 1, length(pl1.sub_location_id)) || pl1.sub_location_id as basin_id,
-          bl2.base_location_id || substr('-', 1, length(pl2.sub_location_id)) || pl2.sub_location_id as primary_stream_id,
-          null as parent_basin_id,
-          b.sort_order,
-          to_number(b.total_drainage_area * cu.factor + cu.offset) as total_drainange_area,
-          to_number(b.contributing_drainage_area * cu.factor + cu.offset) as contributing_drainage_area,
-          cu.to_unit_id as area_unit,
-          b.basin_location_code as basin_code,
-          b.primary_stream_code,
-          b.parent_basin_code
-     from at_basin b,
-          at_base_location bl1,
-          at_physical_location pl1,
-          at_base_location bl2,
-          at_physical_location pl2,
-          cwms_unit_conversion cu,
-          cwms_office o
-    where pl1.location_code = b.basin_location_code
-      and bl1.base_location_code = pl1.base_location_code
-      and o.office_code = bl1.db_office_code
-      and pl2.location_code = b.primary_stream_code
-      and bl2.base_location_code = pl2.base_location_code
-      and b.primary_stream_code is null
-      and cu.from_unit_id = cwms_util.get_default_units('Area', 'SI')
-   union all
-   -- no primary stream but parent basin
-   select o.office_id,
-          bl1.base_location_id || substr('-', 1, length(pl1.sub_location_id)) || pl1.sub_location_id as basin_id,
-          null as primary_stream_id,
-          bl3.base_location_id || substr('-', 1, length(pl3.sub_location_id)) || pl3.sub_location_id as parent_basin_id,
-          b.sort_order,
-          to_number(b.total_drainage_area * cu.factor + cu.offset) as total_drainange_area,
-          to_number(b.contributing_drainage_area * cu.factor + cu.offset) as contributing_drainage_area,
-          cu.to_unit_id as area_unit,
-          b.basin_location_code as basin_code,
-          b.primary_stream_code,
-          b.parent_basin_code
-     from at_basin b,
-          at_base_location bl1,
-          at_physical_location pl1,
-          at_base_location bl3,
-          at_physical_location pl3,
-          cwms_unit_conversion cu,
-          cwms_office o
-    where pl1.location_code = b.basin_location_code
-      and bl1.base_location_code = pl1.base_location_code
-      and o.office_code = bl1.db_office_code
-      and b.primary_stream_code is null
-      and pl3.location_code = b.parent_basin_code
-      and bl3.base_location_code = pl3.base_location_code
-      and cu.from_unit_id = cwms_util.get_default_units('Area', 'SI')
-   union all
-   -- no primary stream and no parent basin
-   select o.office_id,
-          bl1.base_location_id || substr('-', 1, length(pl1.sub_location_id)) || pl1.sub_location_id as basin_id,
-          null as primary_stream_id,
-          null as parent_basin_id,
-          b.sort_order,
-          to_number(b.total_drainage_area * cu.factor + cu.offset) as total_drainange_area,
-          to_number(b.contributing_drainage_area * cu.factor + cu.offset) as contributing_drainage_area,
-          cu.to_unit_id as area_unit,
-          b.basin_location_code as basin_code,
-          b.primary_stream_code,
-          b.parent_basin_code
-     from at_basin b,
-          at_base_location bl1,
-          at_physical_location pl1,
-          cwms_unit_conversion cu,
-          cwms_office o
-    where pl1.location_code = b.basin_location_code
-      and bl1.base_location_code = pl1.base_location_code
-      and o.office_code = bl1.db_office_code
-      and b.primary_stream_code is null
-      and b.parent_basin_code is null
-      and cu.from_unit_id = cwms_util.get_default_units('Area', 'SI');
+/
+
