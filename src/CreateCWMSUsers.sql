410e914cc4fee588f4fc633cd6d9676a66df9511
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2014-07-16 13:34:03 -0800

    Rollback //wcdba/dev/oracle/dev/src/createCWMSUsers.sql to revision 2
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 58536]

--- src/CreateCWMSUsers.sql
+++ src/CreateCWMSUsers.sql
@@ -9,6 +9,7 @@ prompt
 accept echo_state  char prompt 'Enter ON or OFF for echo       : '
 accept inst        char prompt 'Enter the database instance    : '
 accept cwms_schema  char prompt 'Enter cwms schema name    : '
+accept dbi_passwd  char prompt 'Enter the password for cwms  schema   : '
 accept sys_passwd  char prompt 'Enter the password for SYS     : '
 set echo &echo_state
 
@@ -24,12 +25,27 @@ declare
     office_id varchar(32);
     group_id_list "&cwms_schema".char_32_array_type;
 begin
+    for rec in (select dbi_username,office_id from "&cwms_schema".at_sec_dbi_user u,"&cwms_schema".cwms_office c where u.db_office_code = c.OFFICE_code )
+    loop
+        begin
+	    dbms_output.put_line('Creating dbi user ' || rec.dbi_username);
+            "&cwms_schema".CWMS_SEC.CREATE_CWMSDBI_DB_USER(rec.dbi_username,'&dbi_passwd',rec.office_id);
+            exception
+                when others then
+			 dbms_output.put_line( DBMS_UTILITY.FORMAT_ERROR_BACKTRACE);
+        end;
+    end loop;
     for rec in (select at.username,user_db_office_code from "&cwms_schema".at_sec_user_office at, all_users au where at.username=au.username order by username )
     loop
         group_id_list := "&cwms_schema".char_32_array_type();
         select office_id into office_id from "&cwms_schema".cwms_office where office_code = rec.user_db_office_code;
         dbms_output.put_line('Creating ' || rec.username);
        "&cwms_schema".CWMS_SEC.CREATE_USER(rec.username,null,group_id_list,office_id);
+        for drec in (select dbi_username from "&cwms_schema".at_sec_dbi_user)
+        loop
+            execute immediate 'alter user ' || rec.username || ' grant connect through ' || drec.dbi_username;
+	    execute immediate 'alter user ' || rec.username || ' profile CWMS_PROF';
+        end loop;
     end loop;
     commit;
 end;
