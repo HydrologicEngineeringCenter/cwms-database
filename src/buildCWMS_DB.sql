5f7c5f55f9e90bf04cb8dfc4858c8ada4e7c5303
Mike Perryman <michael.d.perryman@usace.army.mil>
2014-07-17 08:54:35 -0800

    Added CWMS-owned text filters
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 58571]

--- src/buildCWMS_DB.sql
+++ src/buildCWMS_DB.sql
@@ -181,6 +181,64 @@ begin
    cwms_text.store_std_text('R', 'HIGH WATER, NO ACCESS');
 end;
 /
+
+begin
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'LOCATION', 
+      p_description    => 'Matches valid CWMS locations', 
+      p_text_filter    => str_tab_t('in:^[^.-]{0,15}[^. -](-[^. -][^.]{0,31})?$', 'ex:^\W', 'ex:\W$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+      
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'BASE_PARAMETER', 
+      p_description    => 'Matches valid CWMS base parameters', 
+      p_text_filter    => str_tab_t('in:^(%|Area|Code|Con[cd]|Count|Currency|Depth|Dir|Dist|Elev|Energy|Evap(Rate)?|Fish|Flow|Frost|Irrad|Opening|pH|Power|Precip|Pres|Rad|Ratio|Speed|SpinRate|Stage|Stor|Temp|Thick|Timing|Travel|Turb[FJN]?|Volt)$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+      
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'PARAMETER', 
+      p_description    => 'Matches valid CWMS parameters', 
+      p_text_filter    => str_tab_t('in:^(%|Area|Code|Con[cd]|Count|Currency|Depth|Dir|Dist|Elev|Energy|Evap(Rate)?|Fish|Flow|Frost|Irrad|Opening|pH|Power|Precip|Pres|Rad|Ratio|Speed|SpinRate|Stage|Stor|Temp|Thick|Timing|Travel|Turb[FJN]?|Volt)(-[^.]{1,32})?$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+      
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'PARAMETER_TYPE', 
+      p_description    => 'Matches valid CWMS parameter types', 
+      p_text_filter    => str_tab_t('in:^(Total|Max|Min|Const|Ave|Inst)$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+      
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'INTERVAL', 
+      p_description    => 'Matches valid CWMS intervals', 
+      p_text_filter    => str_tab_t('in:^(0|~?(1(Minute|Hour|Day|Week|Month|Year|Decade)|([234568]|1[025]|[23]0)Minutes|([23468]|12)Hours|[23456]Days))$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+      
+   cwms_text.store_text_filter(
+      p_text_filter_id => 'DURATION', 
+      p_description    => 'Matches valid CWMS durations', 
+      p_text_filter    => str_tab_t('in:^(0|(1(Minute|Hour|Day|Week|Month|Year|Decade)|([234568]|1[025]|[23]0)Minutes|([23468]|12)Hours|[23456]Days)(BOP)?)$'), 
+      p_fail_if_exists => 'F', 
+      p_uses_regex     => 'T', 
+      p_regex_flags    => null, 
+      p_office_id      => 'CWMS');
+end
+/
+commit;
 --------------------------------------
 -- create and start queues and jobs --
 --------------------------------------
