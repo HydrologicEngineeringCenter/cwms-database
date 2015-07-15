fc3672f848a6017b060c3d90f930521994b2af75
Mike Perryman <michael.d.perryman@usace.army.mil>
2015-07-15 14:37:22 -0800

    Added IS_REGEX column to AV_TEXT_FILTER
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 69013]

--- src/cwms/views/av_text_filter.sql
+++ src/cwms/views/av_text_filter.sql
@@ -12,18 +12,21 @@ insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TEXT_FILTER', nu
  * @field office_id        The office owning the text filter
  * @field text_filter_id   The text identifier (name) of the text filter
  * @field description      The description of the text filter
+ * @field is_regex         A flag (T/F) specifying whether the text filter uses regular expressions (T) or glob wildcards (F)
  */
 ');
 create or replace force view av_text_filter(
    text_filter_code,
    office_id,
    text_filter_id,
-   description)
+   description,
+   is_regex)
 as
      select tf.text_filter_code,
             o.office_id,
             tf.text_filter_id,
-            tf.description
+            tf.description,
+            tf.is_regex
        from at_text_filter tf, cwms_office o
       where o.office_code = tf.office_code
    order by 2, 3;
