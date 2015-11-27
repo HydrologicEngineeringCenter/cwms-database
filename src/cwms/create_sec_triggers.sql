95085a911fd6753689c3dc40ed59c687fc5a8fad
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2015-11-27 15:34:52 -0800

    Add triggers for AT tables (for readonly users)
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 73414]

---
+++ src/cwms/create_sec_triggers.sql
@@ -0,0 +1,34 @@
+DECLARE
+   l_cmd   VARCHAR2 (1024);
+   l_trig VARCHAR2(64);
+BEGIN
+   FOR c IN (SELECT table_name
+               FROM user_tables
+              WHERE table_name LIKE 'AT_%' AND TEMPORARY = 'N')
+   LOOP
+      l_trig := replace(c.table_name,'AT_','TR_');
+      l_cmd :=
+            'CREATE OR REPLACE TRIGGER '
+         || l_trig
+         || ' AFTER DELETE OR INSERT OR UPDATE
+              ON '
+         || c.table_name
+         || ' REFERENCING NEW AS NEW OLD AS OLD
+
+             DECLARE
+    
+             l_priv   VARCHAR2 (16);
+             BEGIN
+             SELECT SYS_CONTEXT (''CWMS_ENV'', ''CWMS_PRIVILEGE'') INTO l_priv FROM DUAL;
+             IF (l_priv = ''READ_ONLY'')
+             THEN
+     
+               CWMS_20.CWMS_ERR.RAISE(''NO_WRITE_PRIVILEGE'');
+     
+             END IF;
+           END;';
+   DBMS_OUTPUT.PUT_LINE(l_cmd);
+   execute immediate l_cmd;
+   END LOOP;
+END;
+/
