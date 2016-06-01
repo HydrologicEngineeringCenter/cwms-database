f19be052cbde9f886d7b93cfb993607bf9394701
Mike Perryman <michael.d.perryman@usace.army.mil>
2016-06-01 12:32:26 -0800

    Added missing AV_ENTTY_CATEGORY view
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 78464]

---
+++ src/cwms/views/av_entity_category.sql
@@ -0,0 +1,24 @@
+------------------------
+-- AV_ENTITY_CATEGORY --
+------------------------
+insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_ENTITY_CATEGORY', null,
+'
+/**
+ * Displays entity categories
+ *
+ * @since CWMS 3.0
+ *
+ * @field category_id The entity category
+ * @field description A description of the category 
+ */
+');
+create or replace force view av_entity_category (
+   category_id,
+   description)
+as 
+select category_id,
+       description
+  from cwms_entity_category;
+
+create or replace public synonym cwms_v_entity_category for av_entity_category;
+
