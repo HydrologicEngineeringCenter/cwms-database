653ea34be1d6a4f2142811f360d65d764f711a50
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2017-05-31 09:27:21 -0800

    Make the VPD policy context sensitive
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 92507]

--- src/cwms/create_service_user_policy.sql
+++ src/cwms/create_service_user_policy.sql
@@ -20,12 +20,22 @@ END CHECK_SESSION_USER;
 BEGIN
     for c IN (select table_name from user_tables WHERE table_name <> 'AT_SEC_SESSION')
     loop
+        BEGIN
+          DBMS_RLS.DROP_POLICY(
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'SERVICE_USER_POLICY');
+       EXCEPTION WHEN OTHERS THEN
+        NULL;
+       END;
+
         DBMS_RLS.ADD_POLICY (
-        object_schema    => '&cwms_schema', 
-        object_name      => c.table_name, 
-        policy_name      => 'SERVICE_USER_POLICY', 
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'SERVICE_USER_POLICY',
         function_schema  => '&cwms_schema',
-        policy_function  => 'CHECK_SESSION_USER', 
+        policy_function  => 'CHECK_SESSION_USER',
+        policy_type      => DBMS_RLS.SHARED_CONTEXT_SENSITIVE,
         statement_types  => 'select');
     end loop;
 END;
