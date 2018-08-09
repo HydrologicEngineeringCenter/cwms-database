bb289e582cc5f8992c53ea4e1d62ca1cac6af2e4
Mike Perryman <michael.d.perryman@usace.army.mil>
2018-08-09 05:21:42 -0800

    Rename/move file(s)
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 114542]

--- src/updateScripts/18_1_1/modify_transitional_virtual_rating_tables.sql
+++
@@ -1,3 +0,0 @@
-alter table at_virtual_rating drop constraint at_virtual_rating_u1;
-alter table at_virtual_rating_element add constraint at_virtual_rating_element_fk2 foreign key (rating_spec_code) references at_rating_spec (rating_spec_code);
-alter table at_transitional_rating drop constraint at_transitional_rating_u1;
