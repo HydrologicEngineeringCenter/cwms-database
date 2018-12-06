4efeee0c94aa3de34f779a45b7502ce7b0d75157
Mike Perryman <michael.d.perryman@usace.army.mil>
2018-12-06 13:48:28 -0800

    Modified how the update script modified the lookup tables and references to them.
    [git-p4: depot-paths = "//wcdba/dev/oracle/v18.1/": change = 121736]

--- src/updateScripts/18_1_1/update_embankment_protection_types.sql
+++ src/updateScripts/18_1_1/update_embankment_protection_types.sql
@@ -1,39 +1,24 @@
 declare
    l_matching_code    integer;
-   l_cwms_office_code integer;
-   l_host_office_code integer;
 begin
-   --------------------------
-   -- get the office codes --
-   --------------------------
-   l_cwms_office_code := cwms_util.db_office_code_all;
-   begin
-      select office_code
-        into l_host_office_code
-        from cwms_office
-       where eroc = (select substr(db_unique_name, 1, 2) from v$database);
-   exception
-      when no_data_found then null;
-   end;
    -------------------------------
    -- insert the common records --
    -------------------------------
-   insert into at_embank_protection_type values(1, l_cwms_office_code, 'Concrete Blanket',    'Protected by blanket of concrete',              'T');
-   insert into at_embank_protection_type values(2, l_cwms_office_code, 'Concrete Arch Facing','Protected by the faces of the concrete arches', 'T');
-   insert into at_embank_protection_type values(3, l_cwms_office_code, 'Masonry Facing',      'Protected by masonry facing',                   'T');
-   insert into at_embank_protection_type values(4, l_cwms_office_code, 'Grass-Covered Soil',  'Protected by grass-covered soil',               'T');
-   insert into at_embank_protection_type values(5, l_cwms_office_code, 'Soil Cement',         'Protected by soil cement',                      'T');
-   insert into at_embank_protection_type values(6, l_cwms_office_code, 'Rock Riprap',         'Protected by rock riprap',                      'T');
-   insert into at_embank_protection_type values(7, l_cwms_office_code, 'Natural Rock',        'Protected by natural rock',                     'T');
-   insert into at_embank_protection_type values(8, l_cwms_office_code, 'Stone Toe',           'Protected by a stone toe',                      'T');
+   insert into at_embank_protection_type values(1, cwms_util.db_office_code_all, 'Concrete Blanket',    'Protected by blanket of concrete',              'T');
+   insert into at_embank_protection_type values(2, cwms_util.db_office_code_all, 'Concrete Arch Facing','Protected by the faces of the concrete arches', 'T');
+   insert into at_embank_protection_type values(3, cwms_util.db_office_code_all, 'Masonry Facing',      'Protected by masonry facing',                   'T');
+   insert into at_embank_protection_type values(4, cwms_util.db_office_code_all, 'Grass-Covered Soil',  'Protected by grass-covered soil',               'T');
+   insert into at_embank_protection_type values(5, cwms_util.db_office_code_all, 'Soil Cement',         'Protected by soil cement',                      'T');
+   insert into at_embank_protection_type values(6, cwms_util.db_office_code_all, 'Rock Riprap',         'Protected by rock riprap',                      'T');
+   insert into at_embank_protection_type values(7, cwms_util.db_office_code_all, 'Natural Rock',        'Protected by natural rock',                     'T');
+   insert into at_embank_protection_type values(8, cwms_util.db_office_code_all, 'Stone Toe',           'Protected by a stone toe',                      'T');
 
-   if l_host_office_code is not null then
       ----------------------------------------------------------------------
       -- delete any local records that are replaced by the common records --
       ----------------------------------------------------------------------
       for rec in (select * 
                     from at_embank_protection_type
-                   where db_office_code = l_cwms_office_code 
+                   where db_office_code = cwms_util.db_office_code_all 
                    order by protection_type_code
                  )
       loop
@@ -45,7 +30,7 @@ begin
               into l_matching_code
               from at_embank_protection_type
              where protection_type_display_value = rec.protection_type_display_value
-               and db_office_code = l_host_office_code;
+               and db_office_code != cwms_util.db_office_code_all;
          exception
             when no_data_found then continue;
          end;   
@@ -66,6 +51,5 @@ begin
            from at_embank_protection_type
           where protection_type_code = l_matching_code; 
       end loop;
-   end if;
 end;   
 /
--- src/updateScripts/18_1_1/update_gate_release_reasons.sql
+++ src/updateScripts/18_1_1/update_gate_release_reasons.sql
@@ -1,64 +1,48 @@
 declare
    l_matching_code    integer;
-   l_cwms_office_code integer;
-   l_host_office_code integer;
 begin
-   --------------------------
-   -- get the office codes --
-   --------------------------
-   l_cwms_office_code := cwms_util.db_office_code_all;
-   begin
-      select office_code
-        into l_host_office_code
-        from cwms_office
-       where eroc = (select substr(db_unique_name, 1, 2) from v$database);
-   exception
-      when no_data_found then null;
-   end;
    -------------------------------
    -- insert the common records --
    -------------------------------
-   insert into at_gate_release_reason_code values(1, l_cwms_office_code, 'F', 'Flood control release', 'T');
-   insert into at_gate_release_reason_code values(2, l_cwms_office_code, 'W', 'Water supply release',  'T');
-   insert into at_gate_release_reason_code values(3, l_cwms_office_code, 'Q', 'Water quality release', 'T');
-   insert into at_gate_release_reason_code values(4, l_cwms_office_code, 'H', 'Hydropower release',    'T');
-   insert into at_gate_release_reason_code values(5, l_cwms_office_code, 'O', 'Other release',         'T');
+   insert into at_gate_release_reason_code values(1, cwms_util.db_office_code_all, 'F', 'Flood control release', 'T');
+   insert into at_gate_release_reason_code values(2, cwms_util.db_office_code_all, 'W', 'Water supply release',  'T');
+   insert into at_gate_release_reason_code values(3, cwms_util.db_office_code_all, 'Q', 'Water quality release', 'T');
+   insert into at_gate_release_reason_code values(4, cwms_util.db_office_code_all, 'H', 'Hydropower release',    'T');
+   insert into at_gate_release_reason_code values(5, cwms_util.db_office_code_all, 'O', 'Other release',         'T');
          
-   if l_host_office_code is not null then
-      ----------------------------------------------------------------------
-      -- delete any local records that are replaced by the common records --
-      ----------------------------------------------------------------------
-      for rec in (select * 
-                    from at_gate_release_reason_code
-                   where db_office_code = l_cwms_office_code 
-                   order by release_reason_code
-                 )
-      loop
-         -------------------------------
-         -- get the local record code --
-         -------------------------------
-         begin
-            select release_reason_code
-              into l_matching_code
-              from at_gate_release_reason_code
-             where release_reason_display_value = rec.release_reason_display_value
-               and db_office_code = l_host_office_code;
-         exception
-            when no_data_found then continue;
-         end;   
-         ----------------------------------------------------------
-         -- update values foreign keyed to the local record code --
-         ----------------------------------------------------------
-         update at_gate_change 
-            set release_reason_code = rec.release_reason_code
-          where release_reason_code = l_matching_code;  
-         -----------------------------
-         -- delete the local record --
-         -----------------------------
-         delete
+   ----------------------------------------------------------------------
+   -- delete any local records that are replaced by the common records --
+   ----------------------------------------------------------------------
+   for rec in (select * 
+                 from at_gate_release_reason_code
+                where db_office_code = cwms_util.db_office_code_all 
+                order by release_reason_code
+              )
+   loop
+      -------------------------------
+      -- get the local record code --
+      -------------------------------
+      begin
+         select release_reason_code
+           into l_matching_code
            from at_gate_release_reason_code
-          where release_reason_code = l_matching_code; 
-      end loop;
-   end if;
+          where release_reason_display_value = rec.release_reason_display_value
+            and db_office_code != cwms_util.db_office_code_all;
+      exception
+         when no_data_found then continue;
+      end;   
+      ----------------------------------------------------------
+      -- update values foreign keyed to the local record code --
+      ----------------------------------------------------------
+      update at_gate_change 
+         set release_reason_code = rec.release_reason_code
+       where release_reason_code = l_matching_code;  
+      -----------------------------
+      -- delete the local record --
+      -----------------------------
+      delete
+        from at_gate_release_reason_code
+       where release_reason_code = l_matching_code; 
+   end loop;
 end;   
 /
