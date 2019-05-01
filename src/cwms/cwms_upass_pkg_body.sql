d88b2b8e24b0c14edd36e038f622d1510d3b04d2
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2019-05-01 15:25:24 -0800

    Unlock user account when UPASS password is updated
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 128548]

--- src/cwms/cwms_upass_pkg_body.sql
+++ src/cwms/cwms_upass_pkg_body.sql
@@ -111,6 +111,8 @@ AS
                 p_phone,
                 p_email,
                 p_principle_name);
+            CWMS_DBA.CWMS_USER_ADMIN.UNLOCK_DB_ACCOUNT(p_userid);
+         commit;
             COMMIT;
         ELSIF ((UPPER (p_control_code)) = 'D')
         THEN
