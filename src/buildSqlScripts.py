65814b57d4c23c145a73620ba5a8f3b98e462992
Mike Perryman <michael.d.perryman@usace.army.mil>
2019-05-07 08:38:13 -0800

    Modified DB build-from-scratch to use unit aliases from the unitConversions python script instead of from separate static AT_SCHEMA_AT_DATA sql script.
    [git-p4: depot-paths = "//wcdba/dev/oracle/dev/": change = 128803]

--- src/buildSqlScripts.py
+++ src/buildSqlScripts.py
@@ -4418,7 +4418,22 @@ for absParam in sorted(unitsByAbsParam.keys()) :
 				"OFFSET"   : offset,
 				"FUNCTION" : function
 			}
+#--------------#
+# Unit aliases #
+#--------------#
+unitIds = set([unitDef[1] for unitDef in unitDefs])
+units_by_alias = {}
+for unit in unitConversions.unit_aliases.keys() :
+	for alias in unitConversions.unit_aliases[unit] :
+		units_by_alias[alias] = unit
 
+with open('data/unit_alias_data.sql', 'w') as f :
+	f.write("begin\n")
+	for alias in sorted(units_by_alias.keys()) :
+		unit = units_by_alias[alias]
+		if not unit in unitIds : continue
+		f.write("\tinsert into at_unit_alias values ('%s', 53, (select unit_code from cwms_unit where unit_id = '%s'));\n" % (alias, unit))
+	f.write("end;\n/\n")
 #--------------#
 # Data quality #
 #--------------#
