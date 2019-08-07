5ae29dbff212d1ca432ef77bd891a2027899296e
Mike Perryman <michael.d.perryman@usace.army.mil>
2019-08-07 05:45:42 -0800

    Adding supporting scripts for updating 18.1.1 to 18.1.2
    [git-p4: depot-paths = "//wcdba/dev/oracle/v18.1/": change = 132890]

---
+++ src/updateScripts/18_1_2/update_db_change_log.sql
@@ -0,0 +1,33 @@
+declare
+   l_database_id varchar2(30);
+begin
+   select nvl(primary_db_unique_name, db_unique_name)
+     into l_database_id
+     from v$database;
+     
+   insert 
+     into cwms_db_change_log
+          (office_code,
+           database_id,
+           application,
+           ver_major,
+           ver_minor,
+           ver_build,
+           ver_date,
+           title,
+           description
+          )
+   values (cwms_util.user_office_code,
+           l_database_id,
+           'CWMS',
+           18,
+           1,
+           1,
+           to_date ('15AUG2018', 'DDMONYYYY'),
+           'CWMS Database Release 18.1.1',
+           'Multiple performance and functionality updates'
+          );
+   commit;
+end;
+/
+
---
+++ src/updateScripts/18_1_2/verify_db_version.sql
@@ -0,0 +1,18 @@
+----------------------------------------------------------
+-- verify that the schema is the version that we expect --
+----------------------------------------------------------
+begin
+   for rec in
+      (select version,
+              to_char(version_date, 'DDMONYYYY') as version_date
+         from &cwms_schema..av_db_change_log
+        where version_date = (select max(version_date) from &cwms_schema..av_db_change_log)
+      )
+   loop
+      if rec.version !=  '18.1.1' :
+        cwms_err.raise('ERROR', 'Expected version 18.1.1 (15AUG2018), got version '||rec.version||' ('||rec.version_date||')');
+      end if;
+   end loop;
+end;
+/
+
