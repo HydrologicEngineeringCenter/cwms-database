0f265123610a51bc496b2d910f9be1247a536577
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2019-10-01 09:47:57 -0800

    1. Updated mv_ts_code_filter to include 'CWMS Mobile' data 2. Additional views for debugging CWMS DB
    [git-p4: depot-paths = "//wcdba/dev/oracle/v18.1/": change = 134937]

---
+++ src/cwms/views/av_ts_msg_archive.sql
@@ -0,0 +1,33 @@
+insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TS_MSG_ARCHIVE', null,
+'
+/**
+ * Displays information about time series data store calls 
+ *
+ * @since CWMS 18.1.3                                                        
+ *
+ * @field cwms_ts_id           Time series ID 
+ * @field db_office_id         Office ID of the time series 
+ * @field message_time         Time at which the data was requested to be stored 
+ * @field first_data_time      Starting date/time of this store call
+ * @field last_data_time       End date/time of this store call 
+ */
+');
+create or replace force view av_ts_msg_archive (cwms_ts_id,
+                db_office_id,
+                message_time,
+                first_data_time,
+                last_data_time
+) as
+	SELECT cwms_ts_id,
+       		db_office_id,
+       		message_time,
+       		first_data_time,
+       		last_data_time
+  	FROM (SELECT * FROM at_ts_msg_archive_1
+        	UNION
+        	SELECT * FROM at_ts_msg_archive_2) m,
+       		at_cwms_ts_id  c
+ 		WHERE c.ts_code = m.TS_CODE;
+
+create or replace public synonym cwms_v_ts_msg_archive for av_ts_msg_archive;
+                                               
---
+++ src/cwms/views/av_tsv_count_day.sql
@@ -0,0 +1,36 @@
+insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TSV_COUNT_DAY', null,
+'
+/**
+ * Displays per day insert/delete/update statistics for AT_TSV tables 
+ *
+ * @since CWMS 18.1.3                                                        
+ *
+ * @field day                  Day on which insert/updates/deletes happened
+ * @field inserts              Number of inserts to TSV tables in the last one day
+ * @field updates              Number of updates to TSV tables in the last one day
+ * @field deletes              Number of deletes to TSV tables in the last one day
+ * @field total                Total number of inserts/updates/deletes to TSV tables in the last one day
+ */
+');
+create or replace force view av_tsv_count_day(
+   day,
+   inserts,
+   updates,
+   deletes,
+   total)                                         
+as
+	SELECT t "Day",
+       		i "Inserts",
+       		u "Updates",
+       		d "Deletes",
+       		i+u+d "Total"
+FROM
+ (SELECT trunc(from_tz(data_entry_date,'UTC') at LOCAL) t,
+         sum(inserts) i,
+         sum(updates) u,
+         sum(deletes) d
+  FROM   at_tsv_count
+  GROUP BY trunc(from_tz(data_entry_date,'UTC') at LOCAL));
+
+create or replace public synonym cwms_v_tsv_count for av_tsv_count_day;
+                                               
