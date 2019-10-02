0d07fd49b4754c6c59ac541638ef33ef282c9b19
Prasad Vemulapati <prasad.vemulapati@usace.army.mil>
2019-10-02 09:34:56 -0800

    Update descriptions
    [git-p4: depot-paths = "//wcdba/dev/oracle/v18.1/": change = 134985]

--- src/cwms/views/av_tsv_count_day.sql
+++ src/cwms/views/av_tsv_count_day.sql
@@ -6,10 +6,10 @@ insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TSV_COUNT_DAY', 
  * @since CWMS 18.1.3                                                        
  *
  * @field day                  Day on which insert/updates/deletes happened
- * @field inserts              Number of inserts to TSV tables in the last one day
- * @field updates              Number of updates to TSV tables in the last one day
- * @field deletes              Number of deletes to TSV tables in the last one day
- * @field total                Total number of inserts/updates/deletes to TSV tables in the last one day
+ * @field inserts              Number of inserts to TSV tables on this day 
+ * @field updates              Number of updates to TSV tables  on this day
+ * @field deletes              Number of deletes to TSV tables on this day 
+ * @field total                Total number of inserts/updates/deletes to TSV tables on this day 
  */
 ');
 create or replace force view av_tsv_count_day(
@@ -32,5 +32,5 @@ FROM
   FROM   at_tsv_count
   GROUP BY trunc(from_tz(data_entry_date,'UTC') at LOCAL));
 
-create or replace public synonym cwms_v_tsv_count for av_tsv_count_day;
+create or replace public synonym cwms_v_tsv_count_day for av_tsv_count_day;
                                                
--- src/cwms/views/av_tsv_count_minute.sql
+++ src/cwms/views/av_tsv_count_minute.sql
@@ -6,9 +6,10 @@ insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TSV_COUNT_MINUTE
  * @since CWMS 18.1.3                                                        
  *
  * @field data_entry_date      Minute at which insert/updates/deletes happened
- * @field inserts              Number of inserts to TSV tables in the last one minute
- * @field updates              Number of updates to TSV tables in the last one minute
- * @field deletes              Number of deletes to TSV tables in the last one minute
+ * @field inserts              Number of inserts to TSV tables in this minute 
+ * @field updates              Number of updates to TSV tables in this minute 
+ * @field deletes              Number of deletes to TSV tables in this minute 
+ * @field total                Total number of inserts/updates/deletes to TSV tables in this minute 
  */
 ');
 create or replace force view av_tsv_count_minute(
@@ -25,5 +26,5 @@ as
 	  inserts+updates+deletes total
      from (select data_entry_date,inserts,updates,deletes from at_tsv_count) ;
 
-create or replace public synonym cwms_v_tsv_count for av_tsv_count_minute;
+create or replace public synonym cwms_v_tsv_count_minute for av_tsv_count_minute;
                                                
