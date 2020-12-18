fb9afa5f10853cdc496d0be9d802b15b26ce6494
Mike Neilson <michael.a.neilson@usace.army.mil>
2020-12-18 07:21:57 -0800

    fixed shell variable subsition now that it's in a seperate file.

--- .teamcity/scripts/create_database.sh
+++ .teamcity/scripts/create_database.sh
@@ -1,10 +1,10 @@
 #/bin/bash
-sqlplus sys/${'$'}SYS_PASSWORD@${'$'}HOST_AND_PORT/${'$'}ContainerDB as SYSDBA <<EOF | grep "ORA-65012: Pluggable database REFS_HEADS_MASTER already exists"
-alter session set PDB_FILE_NAME_CONVERT='/opt/oracle/oradata/ROOTDB/CWMSBASE/','/opt/oracle/oradata/ROOTDB/${'$'}{CWMS_PDB}/';
-CREATE PLUGGABLE DATABASE ${'$'}CWMS_PDB from CWMSBASE;
-ALTER PLUGGABLE DATABASE ${'$'}CWMS_PDB OPEN READ WRITE;
+sqlplus sys/$SYS_PASSWORD@$HOST_AND_PORT/$ContainerDB as SYSDBA <<EOF | grep "ORA-65012: Pluggable database REFS_HEADS_MASTER already exists"
+alter session set PDB_FILE_NAME_CONVERT='/opt/oracle/oradata/ROOTDB/CWMSBASE/','/opt/oracle/oradata/ROOTDB/${CWMS_PDB}/';
+CREATE PLUGGABLE DATABASE $CWMS_PDB from CWMSBASE;
+ALTER PLUGGABLE DATABASE $CWMS_PDB OPEN READ WRITE;
 EOF
-if [ ${'$'}? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     echo "Database wasn't correctly destroyed" 1>&2
     exit 1
 fi
--- .teamcity/scripts/setup_parameters.sh
+++ .teamcity/scripts/setup_parameters.sh
@@ -6,33 +6,33 @@ mkdir output
 export PW=`tr -cd '[:alnum:]' < /dev/urandom | fold -w25 | head -n1`
 
 export CWMS_PDB=`echo %teamcity.build.branch% | sed -e "s@/refs/heads/@@g" | sed "s@/@_@g" | sed "s@\.@_@g"`
-if [[ ${'$'}CWMS_PDB =~ ^[0-9]+${'$'} ]]; then
+if [[ $CWMS_PDB =~ ^[0-9]+$ ]]; then
     echo "prefixing pull request number with text."
-    export CWMS_PDB="PULLREQUEST_${'$'}{CWMS_PDB}"
-elif [[ ${'$'}CWMS_PDB =~ ^[0-9] ]]; then
+    export CWMS_PDB="PULLREQUEST_${CWMS_PDB}"
+elif [[ $CWMS_PDB =~ ^[0-9] ]]; then
     # while not a pull request, it still needs an alpha prefix.
     echo "prefixing with letter"
-    export CWMS_PDB="z_${'$'}{CWMS_PDB}"
+    export CWMS_PDB="z_${CWMS_PDB}"
 fi
 cat <<EOF
-##teamcity[setParameter name='env.CWMS_PDB' value='${'$'}CWMS_PDB']
+##teamcity[setParameter name='env.CWMS_PDB' value='$CWMS_PDB']
 
 EOF
 
-echo "=${'$'}CWMS_PDB="
+echo "=$CWMS_PDB="
 
-sed -e "s/SYS_PASSWORD/${'$'}SYS_PASSWORD/g" \
-    -e "s/PASSWORD/${'$'}PW/g" \
-    -e "s/HOST_AND_PORT/${'$'}HOST_AND_PORT/g" \
-    -e "s/SERVICE_NAME/${'$'}CWMS_PDB/g" \
-    -e "s/OFFICE_ID/${'$'}OFFICE_ID/g" \
-    -e "s/OFFICE_CODE/${'$'}OFFICE_CODE/g" \
+sed -e "s/SYS_PASSWORD/$SYS_PASSWORD/g" \
+    -e "s/PASSWORD/$PW/g" \
+    -e "s/HOST_AND_PORT/$HOST_AND_PORT/g" \
+    -e "s/SERVICE_NAME/$CWMS_PDB/g" \
+    -e "s/OFFICE_ID/$OFFICE_ID/g" \
+    -e "s/OFFICE_CODE/$OFFICE_CODE/g" \
     -e "s/TEST_ACCOUNT_FLAG/-testaccount/g" teamcity_overrides.xml > output/overrides.xml
 cat <<EOF
-##teamcity[setParameter name='env.CWMS_PASSWORD' value='${'$'}PW']
+##teamcity[setParameter name='env.CWMS_PASSWORD' value='$PW']
 
 EOF
 
-echo "${'$'}CWMS_PDB" > output/database.info
-echo "${'$'}PW" >> output/database.info
+echo "$CWMS_PDB" > output/database.info
+echo "$PW" >> output/database.info
 exit 0
