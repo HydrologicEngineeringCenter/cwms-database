e3e4938ca7165f50ebea8fcc278163e5ebc88727
mperryman <michael.d.perryman@usace.army.mil>
2021-02-18 14:03:14 -0600

    Adding update script

---
+++ src/updateScripts/18_1_10/update_db_change_log.sql
@@ -0,0 +1,33 @@
+declare
+   l_database_id varchar2(30);
+begin
+   select nvl(primary_db_unique_name, db_unique_name)
+     into l_database_id
+     from v$database;
+
+   insert
+     into cwms_db_change_log
+          (office_code,
+           database_id,
+           application,
+           ver_major,
+           ver_minor,
+           ver_build,
+           ver_date,
+           title,
+           description
+          )
+   values (cwms_util.user_office_code,
+           l_database_id,
+           'CWMS',
+           18,
+           1,
+           10,
+           to_date ('18FEB2021', 'DDMONYYYY'),
+           'CWMS Database Release 18.1.10',
+           'Bug fixes to 18.1.9'
+          );
+   commit;
+end;
+/
+
