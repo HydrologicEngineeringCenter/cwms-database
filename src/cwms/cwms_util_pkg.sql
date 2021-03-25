1fed8f19d12be83af055de0ee4391efcb8262634
Perryman <1230931808121002@mil>
2021-03-25 16:07:51 -0500

    updating cwms_util spec (again)

--- src/cwms/cwms_util_pkg.sql
+++ src/cwms/cwms_util_pkg.sql
@@ -214,19 +214,19 @@ AS
     * Contains names of all constants that can be used with expression evaluation.
     */
    expression_constants str_tab_t := str_tab_t('E','PI');
-   function get_expression_constants;
+   function get_expression_constants return str_tab_t;
    /**
     * Contains all mathematical operators that can be used with expression evaluation.
     */
    expression_operators str_tab_t := str_tab_t('+','-','*','/','//','%','^');
-   function get_expression_operators;
+   function get_expression_operators return str_tab_t;
    /**
     * Contains names of all functions that can be used with expression evaluation.
     */
    expression_functions str_tab_t := str_tab_t(
       'ABS','ACOS','ASIN','ATAN','CEIL','COS','EXP','FLOOR',
       'INV','LN','LOG','NEG','ROUND','SIGN','SIN','SQRT','TAN','TRUNC');
-   function get_expression_functions;
+   function get_expression_functions return str_tab_t;
    /**
     * Contains all valid logical comparision operators
     */
