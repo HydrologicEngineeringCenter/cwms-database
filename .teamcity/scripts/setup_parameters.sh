ed9a5803ed26d975080325bf9be94c59e4aa8716
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-07-28 10:42:46 -0700

    Moved away from using mvn to get version.

--- .teamcity/scripts/setup_parameters.sh
+++ .teamcity/scripts/setup_parameters.sh
@@ -18,7 +18,7 @@ fi
 if [ "$IS_DEPLOY" == "1" ]; then
     # this is a deployment, modifiy the PDB name to include the version identified so
     # additional merges to master can happen independently.
-    version=`mvn -q -Dexec.executable=echo -Dexec.args='\${project.version}' --non-recursive exec:exec | sed "s@-@_@g"`
+    version=`ant version |tail -1 | sed -e 's@^.*\[echo\]\s*\(.*$\)@\1@' | sed "s@-@_@g"`
     CWMS_PDB="${CWMS_PDB}_$version"
 fi
 
