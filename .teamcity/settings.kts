04bbe7f543c78e00423754c9a7f9125051cbe01c
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-10-06 08:47:59 -0700

    [build:docker] modified how docker.showport behaves.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -94,11 +94,6 @@ object Build : BuildType({
             antArguments = "-Dteamcity.branch=%teamcity.build.branch%"
         }
         ant {
-            name = "Generate Overrides"
-            targets = "docker.generateoverrides"
-            antArguments = "-Dteamcity.branch=%teamcity.build.branch%"
-        }
-        ant {
             name = "Install CWMS Database"
             targets = "install"
             antArguments = "-Dbuilduser.overrides=build/overrides.external.xml"
