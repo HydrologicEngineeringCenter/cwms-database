dd8f9da88495ffcf102297ffa9df07908b9892c5
Mike Neilson <michael.a.neilson@usace.army.mil>
2022-01-31 13:53:05 -0800

    baseline move of schema.

---
+++ schema/src/main/resources/db/migration/V_/2022/1.1/create_service_user_policy.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE FUNCTION CHECK_SESSION_USER(
+  schema_p   IN VARCHAR2,
+  table_p    IN VARCHAR2)
+ RETURN VARCHAR2
+ AS
+  
+ BEGIN
+   if dbms_mview.i_am_a_refresh then
+   return null;
+  end if;
+  IF ((SYS_CONTEXT ('CWMS_ENV', 'CWMS_USER') IS NULL) AND (USER = cwms_sec.cac_service_user))
+  THEN
+    return '1=0';
+  ELSE
+    return '1=1';
+  END IF;
+END CHECK_SESSION_USER;
+/
+
+BEGIN
+    for c IN (select table_name from user_tables WHERE table_name <> 'AT_SEC_SESSION')
+    loop
+        BEGIN
+          DBMS_RLS.DROP_POLICY(
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'SERVICE_USER_POLICY');
+       EXCEPTION WHEN OTHERS THEN
+        NULL;
+       END;
+
+        DBMS_RLS.ADD_POLICY (
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'SERVICE_USER_POLICY',
+        function_schema  => '&cwms_schema',
+        policy_function  => 'CHECK_SESSION_USER',
+        policy_type      => DBMS_RLS.SHARED_CONTEXT_SENSITIVE,
+        statement_types  => 'select');
+    end loop;
+END;
+/
+
