1a785fa0d5bfd5fd5c58d8ab77decf2004c2ece5
Mike Neilson <michael.a.neilson@usace.army.mil>
2022-02-01 10:39:41 -0800

    migrated role creation.

---
+++ schema/src/main/resources/db/migration/V2022/1/V2022.1.1.004__cwms_user_role.sql
@@ -0,0 +1,2 @@
+
+create role cwms_user not identified;
--- schema/src/main/resources/db/migration/callbacks/afterClean.sql
+++ schema/src/main/resources/db/migration/callbacks/afterClean.sql
@@ -1,1 +1,15 @@
 DROP PROFILE cwms_prof cascade;
+drop role cwms_user;
+
+begin
+   for rec in (select object_name
+                 from dba_objects
+                where owner = '${CWMS_SCHEMA}'
+                  and object_type = 'TYPE'
+                  and object_name not like 'SYS\_%' escape '\'
+             order by object_name)
+   loop
+      dbms_output.put_line('Dropping type '||rec.object_name);
+      execute immediate 'drop type '||rec.object_name||' force';
+   end loop;
+end;
