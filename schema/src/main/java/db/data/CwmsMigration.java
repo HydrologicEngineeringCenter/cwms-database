115a6c71e777ee572f6d2bfa10836032c11bc54e
Mike Neilson <michael.a.neilson@usace.army.mil>
2022-10-06 20:50:36 +0000

    default mapper with correct settings.

--- schema/src/main/java/db/data/CwmsMigration.java
+++ schema/src/main/java/db/data/CwmsMigration.java
@@ -2,7 +2,16 @@ package db.data;
 
 import java.io.InputStream;
 
+import com.fasterxml.jackson.core.JsonParser.Feature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public interface CwmsMigration {
+    static final ObjectMapper defaultMapper = new ObjectMapper().enable(Feature.ALLOW_COMMENTS);
+
+    default public ObjectMapper getDefaultMapper() {
+        return defaultMapper;
+    }
+
     default public InputStream getData(String resourceName) {
         return this.getClass().getClassLoader().getResourceAsStream(resourceName);
     }
