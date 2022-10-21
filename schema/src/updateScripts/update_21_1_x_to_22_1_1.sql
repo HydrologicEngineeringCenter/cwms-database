41fcb60fae6ce59451b69416d533d0fd3b5e56f9
Mike Perryman <michael.d.perryman@usace.army.mil>
2022-10-21 14:12:04 +0000

    Updating update script from 21.1.x

--- schema/src/updateScripts/update_21_1_x_to_22_1_1.sql
+++ schema/src/updateScripts/update_21_1_x_to_22_1_1.sql
@@ -46,108 +46,100 @@ whenever sqlerror exit;
 column db_name new_value db_name
 select :db_name as db_name from dual;
 define logfile=update_&db_name._21_1_x_to_22_1_1.log
-PROMPT log file = &logfile
+prompt log file = &logfile
 spool &logfile append;
 -------------------
 -- do the update --
 -------------------
-PROMPT ################################################################################
-PROMPT VERIFYING EXPECTED VERSION
+prompt ################################################################################
+prompt VERIFYING EXPECTED VERSION
 select systimestamp from dual;
 @@./22_1_1/verify_db_version
-PROMPT ################################################################################
+prompt ################################################################################
+prompt CREATING AND ALTERING TABLES
+select systimestamp from dual;
 
-PROMPT ********** store existing privileges to non-CWMS users
-@@./util/preupdate_privs.sql
-
-PROMPT ********** Update cwms tables
-@@./22_1_1/update_cwms_tables
-
-PROMPT ********** Alter table(s)
 whenever sqlerror continue;
-ALTER TABLE AT_LOC_LVL_INDICATOR_COND DROP CONSTRAINT AT_LOC_LVL_INDICATOR_COND_CK8;
-whenever sqlerror exit;
-BEGIN
-    EXECUTE IMMEDIATE 'ALTER TABLE AT_LOC_LVL_INDICATOR_COND MODIFY(COMPARISON_UNIT  NOT NULL)';
-EXCEPTION
-    WHEN OTHERS
-    THEN
-        IF SQLCODE = -1442
-        THEN
-            DBMS_OUTPUT.PUT_LINE ('Column is already set to not null');
-            NULL;     
-        ELSE
-            RAISE;
-        END IF;
-END;
-/
-ALTER TABLE AT_LOC_LVL_INDICATOR_COND
- ADD CONSTRAINT AT_LOC_LVL_INDICATOR_COND_CK8
-  CHECK (NOT(RATE_EXPRESSION IS NOT NULL AND RATE_COMPARISON_UNIT IS NULL));
+alter table at_loc_lvl_indicator_cond modify comparison_unit number(14) not null;
+alter table at_loc_lvl_indicator_cond add constraint at_loc_lvl_indicator_cond_ck8 check (not(rate_expression is not null and rate_comparison_unit is null));
 
-PROMPT ********** Add/update comments
-COMMENT ON COLUMN AT_LOC_LVL_INDICATOR_COND.COMPARISON_UNIT IS 'Unit of V, L (or L1), and L2 used for comparisons. Not necessarliy the unit of the expression result';
-COMMENT ON COLUMN AT_LOC_LVL_INDICATOR_COND.RATE_COMPARISON_UNIT IS 'Unit of V, L (or L1), and L2 used for rate comparisons. The numerator unit for R (e.g., ft3 for R in cfs [ft3/s])';
+insert into cwms_abstract_parameter values (35, 'Depth Velocity');
 
-PROMPT ********** Build new index for CWMS_TIME_ZONE
-whenever sqlerror continue;
-DROP INDEX CWMS_TIME_ZONE_TNU;
-whenever sqlerror exit;
-CREATE UNIQUE INDEX CWMS_TIME_ZONE_TNU ON CWMS_TIME_ZONE(UPPER("TIME_ZONE_NAME"));
+insert into cwms_unit values(130, 'm2/s',  35, 'SI', 'Depth times velocity', 'Depth of 1 meter and Velocity of 1 meter/second');
+insert into cwms_unit values(131, 'ft2/s', 35, 'EN', 'Depth times velocity', 'Depth of 1 feet and Velocity of 1 feet/second');
+insert into cwms_unit values(132, 'ug',    32, 'SI', 'micrograms',           'Mass of 1 microgram');
+insert into cwms_unit values(133, 'ug/l',  18, 'SI', 'micrograms per liter', 'Mass concentration of 1E-06 gram per liter');
 
------------
--- VIEWS --
------------
-PROMPT ********** Updating views
+insert into at_unit_alias values('DEGF', 53, 68);
+insert into at_unit_alias values('micrograms', 53, 132);
+insert into at_unit_alias values('micrograms/L', 53, 133);
+insert into at_unit_alias values('micrograms/l', 53, 133);
 
-delete from at_clob where id = '/VIEWDOCS/AV_LOCATION_LEVEL';
-@../cwms/views/av_location_level
-delete from at_clob where id = '/VIEWDOCS/AV_LOCATION_LEVEL2';
-@../cwms/views/av_location_level2
-delete from at_clob where id = '/VIEWDOCS/AV_PARAMETER_TYPE';
-@../cwms/views/av_parameter_type
-delete from at_clob where id = '/VIEWDOCS/AV_VIRTUAL_LOCATION_LEVEL';
-@../cwms/views/av_virtual_location_level
-delete from at_clob where id = '/VIEWDOCS/AV_VLOC_LVL_CONSTITUENT';
-@../cwms/views/av_vloc_lvl_constituent
-commit;
+insert into cwms_unit_conversion values('ft2/s',  'ft2/s',  35, 131, 131, 1.0,                   0.0, null);
+insert into cwms_unit_conversion values('ft2/s',  'm2/s',   35, 131, 130, 0.09290304,            0.0, null);
+insert into cwms_unit_conversion values('g',      'ug',     32, 115, 132, 1000000.0,             0.0, null);
+insert into cwms_unit_conversion values('g/l',    'ug/l',   18,  49, 133, 1000000.0,             0.0, null);
+insert into cwms_unit_conversion values('gm/cm3', 'ug/l',   18,  50, 133, 1000000000.0,          0.0, null);
+insert into cwms_unit_conversion values('kg',     'ug',     32, 116, 132, 1000000000.0,          0.0, null);
+insert into cwms_unit_conversion values('lbm',    'ug',     32, 117, 132, 453592370.0,           0.0, null);
+insert into cwms_unit_conversion values('m2/s',   'ft2/s',  35, 130, 131, 10.76391041670972,     0.0, null);
+insert into cwms_unit_conversion values('m2/s',   'm2/s',   35, 130, 130, 1.0,                   0.0, null);
+insert into cwms_unit_conversion values('mg',     'ug',     32, 118, 132, 1000.0,                0.0, null);
+insert into cwms_unit_conversion values('mg/l',   'ug/l',   18,  51, 133, 1000.0,                0.0, null);
+insert into cwms_unit_conversion values('ppm',    'ug/l',   18,  52, 133, 1000.0,                0.0, null);
+insert into cwms_unit_conversion values('ton',    'ug',     32, 119, 132, 907184740000.0,        0.0, null);
+insert into cwms_unit_conversion values('tonne',  'ug',     32, 120, 132, 1000000000000.0,       0.0, null);
+insert into cwms_unit_conversion values('ug',     'g',      32, 132, 115, 1e-06,                 0.0, null);
+insert into cwms_unit_conversion values('ug',     'kg',     32, 132, 116, 1e-09,                 0.0, null);
+insert into cwms_unit_conversion values('ug',     'lbm',    32, 132, 117, 2.204622621848776e-09, 0.0, null);
+insert into cwms_unit_conversion values('ug',     'mg',     32, 132, 118, 0.001,                 0.0, null);
+insert into cwms_unit_conversion values('ug',     'ton',    32, 132, 119, 1.102311310924388e-12, 0.0, null);
+insert into cwms_unit_conversion values('ug',     'tonne',  32, 132, 120, 1e-12,                 0.0, null);
+insert into cwms_unit_conversion values('ug',     'ug',     32, 132, 132, 1.0,                   0.0, null);
+insert into cwms_unit_conversion values('ug/l',   'g/l',    18, 133,  49, 1e-06,                 0.0, null);
+insert into cwms_unit_conversion values('ug/l',   'gm/cm3', 18, 133,  50, 1e-09,                 0.0, null);
+insert into cwms_unit_conversion values('ug/l',   'mg/l',   18, 133,  51, 0.001,                 0.0, null);
+insert into cwms_unit_conversion values('ug/l',   'ppm',    18, 133,  52, 0.001,                 0.0, null);
+insert into cwms_unit_conversion values('ug/l',   'ug/l',   18, 133, 133, 1.0,                   0.0, null);
 
--------------------
--- PACKAGE SPECS --
--------------------
-PROMPT ********** update package specs
+insert into cwms_base_parameter values (49, 'DepthVelocity', 35, 130, 130, 131, 'Depth Velocity', 'Depth Velocity');
+
+insert into at_parameter values(49, 53, 49, null, 'Depth Velocity');
+
+create unique index cwms_time_zone_tnu on cwms_time_zone(upper(time_zone_name));
+
+prompt ################################################################################
+prompt REMOVING REMOVE_DEAD_SUBSCRIBERS JOB
+select systimestamp from dual;
+@@../cwms/remove_dead_subscribers
+
+prompt ################################################################################
+prompt CREATING AND ALTERING PACKAGE SPECIFICATIONS
+select systimestamp from dual;
 
 @../cwms/cwms_level_pkg
 @../cwms/cwms_loc_pkg
 @../cwms/cwms_sec_pkg
 @../cwms/cwms_sec_policy
 @../cwms/cwms_util_pkg
+alter session set current_schema = &cwms_dba_schema;
+@../cwms_dba/cwms_user_admin_pkg
+alter session set current_schema = &cwms_schema;
 
-----------------
--- TYPE SPECS --
-----------------
-PROMPT ********** update type specs
+prompt ################################################################################
+prompt CREATING AND ALTERING TYPE SPECIFICATIONS
+select systimestamp from dual;
 
-drop type location_level_t force;
-@../cwms/types/location_level_t
+drop type location_level_t    force;
 drop type loc_lvl_indicator_t force;
+drop type zlocation_level_t   force;
+@../cwms/types/location_level_t
 @../cwms/types/loc_lvl_indicator_t
-drop type zlocation_level_t; 
 @../cwms/types/zlocation_level_t
 
-----------------
--- TYPE BODY --
-----------------
-PROMPT ********** update type bodies
-
-@../cwms/types/location_level_t-body
-@../cwms/types/loc_lvl_indicator_cond_t-body
-@../cwms/types/loc_lvl_indicator_t-body
-@../cwms/types/zlocation_level_t-body
---------------------
--- PACKAGE BODIES --
---------------------
-PROMPT ********** update package bodies
+prompt ################################################################################
+prompt CREATING AND ALTERING PACKAGE BODIES
+select systimestamp from dual;
 
 @../cwms/cwms_display_pkg_body
 @../cwms/cwms_level_pkg_body
@@ -158,40 +150,74 @@ PROMPT ********** update package bodies
 @../cwms/cwms_ts_pkg_body
 @../cwms/cwms_ts_id_pkg_body
 @../cwms/cwms_util_pkg_body
+alter session set current_schema = &cwms_dba_schema;
+@../cwms_dba/cwms_user_admin_pkg_body
+alter session set current_schema = &cwms_schema;
 
-PROMPT ********** Drop objects
-whenever sqlerror continue;
-drop function check_session_user;
-drop procedure remove_dead_subscribers;
-whenever sqlerror exit;
+prompt ################################################################################
+prompt CREATING AND ALTERING TYPE BODIES
+select systimestamp from dual;
 
-PROMPT ********** Grant pemissions
+@../cwms/types/location_level_t-body;
+@../cwms/types/loc_lvl_indicator_cond_t-body;
+@../cwms/types/loc_lvl_indicator_t-body;
+@../cwms/types/zlocation_level_t-body;
 
-grant execute on location_level_t to cwms_user;
-grant execute on loc_lvl_indicator_t to cwms_user;
-grant execute on zlocation_level_t to cwms_user;
-grant select on av_parameter_type to cwms_user;
+prompt ################################################################################
+prompt CREATING AND ALTERING VIEWS
+select systimestamp from dual;
 
+delete from at_clob where id = '/VIEWDOCS/AV_TSV';
+delete from at_clob where id = '/VIEWDOCS/AV_TSV_DQU';
+delete from at_clob where id = '/VIEWDOCS/AV_TSV_DQU_30D';
+delete from at_clob where id = '/VIEWDOCS/AV_TSV_DQU_24H';
+delete from at_clob where id = '/VIEWDOCS/AV_LOCATION_LEVEL';
+delete from at_clob where id = '/VIEWDOCS/AV_LOCATION_LEVEL2';
+delete from at_clob where id = '/VIEWDOCS/AV_PARAMETER_TYPE';
+delete from at_clob where id = '/VIEWDOCS/AV_VIRTUAL_LOCATION_LEVEL';
+delete from at_clob where id = '/VIEWDOCS/AV_VLOC_LVL_CONSTITUENT';
+@../cwms/at_schema_tsv_dqu
+@../cwms/views/av_location_level
+@../cwms/views/av_location_level2
+@../cwms/views/av_parameter_type
+@../cwms/views/av_virtual_location_level
+@../cwms/views/av_vloc_lvl_constituent
+create or replace public synonym CWMS_V_PARAMETER_TYPE for AV_PARAMETER_TYPE;
 
-PROMPT ********** Drop application triggers for read only users 
+prompt ################################################################################
+prompt ENSURING GRANTS TO CWMS_USER AND CWMS_DBA
+select systimestamp from dual;
 
-BEGIN
-    FOR c
-        IN (SELECT *
-              FROM dba_objects
-             WHERE     owner = '&cwms_schema'
-                   AND object_type = 'TRIGGER'
-                   AND object_name IN
-                           ('ST_APPLICATION_LOGIN', 'ST_APPLICATION_SESSION'))
-    LOOP
-        DBMS_OUTPUT.PUT_LINE ('drop trigger ' || c.object_name);
-        EXECUTE IMMEDIATE 'drop trigger ' || c.object_name;
-    END LOOP;
-END;
+begin 
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'PACKAGE BODY') loop
+      begin
+         execute immediate 'grant execute on &cwms_schema..'||rec.object_name||' to cwms_user';
+         execute immediate 'grant execute on &cwms_schema..'||rec.object_name||' to &cwms_dba_schema';
+      exception
+         when others then null;
+      end;
+   end loop;
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'TYPE') loop
+      begin
+         execute immediate 'grant execute on &cwms_schema..'||rec.object_name||' to cwms_user';
+         execute immediate 'grant execute on &cwms_schema..'||rec.object_name||' to &cwms_dba_schema';
+      exception
+         when others then null;
+      end;
+   end loop;
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'VIEW' and object_name not like '%AQ$%') loop
+      begin
+         execute immediate 'grant select on &cwms_schema..'||rec.object_name||' to cwms_user';
+         execute immediate 'grant select on &cwms_schema..'||rec.object_name||' to &cwms_dba_schema';
+      exception
+         when others then null;
+      end;
+   end loop;
+end;
 /
 
-PROMPT ################################################################################
-PROMPT INVALID OBJECTS...
+prompt ################################################################################
+prompt INVALID OBJECTS...
 select systimestamp from dual;
 set pagesize 100
 select owner||'.'||substr(object_name, 1, 30) as invalid_object,
@@ -200,21 +226,27 @@ select owner||'.'||substr(object_name, 1, 30) as invalid_object,
  where status = 'INVALID'
    and owner in ('&cwms_schema', '&cwms_dba_schema')
  order by 1, 2;
-PROMPT ################################################################################
-PROMPT ********** RECOMPILING SCHEMA
+
+prompt ################################################################################
+prompt RECOMPILING SCHEMA
 select systimestamp from dual;
 @./util/compile_objects
-PROMPT ********** retstart remove subscribers job
-exec cwms_msg.start_remove_subscribers_job()
-PROMPT ********** create user policies
-@../cwms/create_user_policies.sql
-PROMPT ********** create read only triggers
+
+prompt ################################################################################
+prompt REINSTATING ENTITIES
+select systimestamp from dual;
 @../cwms/create_sec_triggers
-PROMPT ********** RECOMPILING SCHEMA
+@../cwms/at_tsv_count_trig
+@../cwms/at_dd_flag_trig
+
+prompt ################################################################################
+prompt RECOMPILING SCHEMA
 select systimestamp from dual;
 @./util/compile_objects
-PROMPT ################################################################################
-PROMPT ********** REMAINING INVALID OBJECTS...
+
+promp ################################################################################
+prompt REMAINING INVALID OBJECTS...
+select systimestamp from dual;
 select owner||'.'||substr(object_name, 1, 30) as invalid_object,
        object_type
   from all_objects
@@ -229,15 +261,26 @@ select owner||'.'||substr(name, 1, 30) as name,
  where attribute = 'ERROR'
    and owner in ('&cwms_schema', '&cwms_dba_schema')
  order by owner, type, name, sequence;
-PROMPT ################################################################################
-PROMPT ********** restore non-CWMS objects privileges
+prompt ################################################################################
+prompt RESTORE CCP PRIVILEGES
 select systimestamp from dual;
 whenever sqlerror continue;
-@@./util/restore_privs
+declare
+  l_count NUMBER;
+begin
+   select count(*) into l_count from dba_users where username='CCP';
+   if(l_count>0)
+   then
+     for rec in (select object_name from user_objects where object_type in ('PACKAGE', 'TYPE')) loop
+        execute immediate 'grant execute on '||rec.object_name||' to ccp';
+     end loop;
+   end if;
+end;
+/
 whenever sqlerror exit;
-PROMPT ################################################################################
-PROMPT ################################################################################
-PROMPT UPDATING DB_CHANGE_LOG
+prompt ################################################################################
+prompt ################################################################################
+prompt UPDATING DB_CHANGE_LOG
 select systimestamp from dual;
 @@./22_1_1/update_db_change_log
 select substr(version, 1, 10) as version,
@@ -246,8 +289,8 @@ select substr(version, 1, 10) as version,
   from av_db_change_log
  where application = 'CWMS'
  order by version_date;
-PROMPT ################################################################################
-PROMPT ********** UPDATE COMPLETE
+prompt ################################################################################
+prompt UPDATE COMPLETE
 select systimestamp from dual;
 exit
 
