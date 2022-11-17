c9622b6f7cf3ccf4690daf92f01508d864736ff0
Mike Perryman <michael.d.perryman@usace.army.mil>
2022-11-17 18:48:45 +0000

    Added polices to hide new parameter types, durations, and intervals

--- schema/src/cwms/create_user_policies.sql
+++ schema/src/cwms/create_user_policies.sql
@@ -20,7 +20,27 @@ BEGIN
         statement_types  => 'select');
     end loop;
 
-    -- allow PD and DBA users to view the av_sec views
+    for c IN (select table_name from user_tables WHERE table_name in ('CWMS_DURATION','CWMS_INTERVAL','CWMS_PARAMETER_TYPE'))
+    loop
+        BEGIN
+          DBMS_RLS.DROP_POLICY(
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'FILTER_'||c.table_name||'_POLICY');
+       EXCEPTION WHEN OTHERS THEN
+        NULL;
+       END;
+
+        DBMS_RLS.ADD_POLICY (
+        object_schema    => '&cwms_schema',
+        object_name      => c.table_name,
+        policy_name      => 'FILTER_'||c.table_name||'_POLICY', 
+        function_schema  => '&cwms_schema',
+        policy_function  => 'CWMS_SEC_POLICY.'||c.table_name||'_FILTER',
+        policy_type      => DBMS_RLS.SHARED_CONTEXT_SENSITIVE,
+        statement_types  => 'select');
+    end loop;
+
     for c IN (select view_name from user_views where view_name like 'AV_SEC%')
     loop
         BEGIN
