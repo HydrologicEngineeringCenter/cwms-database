60de452e3e540be3c498dbcf200969834b5348dc
Mike Perryman <michael.d.perryman@usace.army.mil>
2022-11-29 23:42:44 +0000

    Modify test to better test cat_ts_transfer routine

--- schema/src/test/test_cwms_data_dissem.sql
+++ schema/src/test/test_cwms_data_dissem.sql
@@ -3,8 +3,7 @@ create or replace package test_cwms_data_dissem as
 --%rollback(manual)
 --%beforeall(setup)
 --%afterall(teardown)
-
---%test(Test group- and filter-based dissemenation routnies)
+--%test(Test group- and filter-based dissemenation routines)
 procedure test_data_dissem;
 procedure setup;
 procedure teardown;
@@ -82,15 +81,21 @@ end setup;
 --------------------------------------------------------------------------------
 procedure test_data_dissem
 is
-   l_seq         pls_integer := 0;
-   l_ts_code     cwms_v_ts_id.ts_code%type;
-   l_crsr        sys_refcursor;
-   l_cwms_ts_id  cwms_v_ts_id.cwms_ts_id%type;
-   l_public_name cwms_v_loc.public_name%type;
-   l_office_id   cwms_v_ts_id.db_office_id%type;
-   l_office_code cwms_v_ts_id.db_office_code%type;
-   l_dest_db     varchar2(16);
-   function in_group(p_group_id in varchar2) return boolean
+   l_seq          pls_integer := 0;
+   l_ts_id        cwms_v_ts_id.cwms_ts_id%type;
+   l_ts_code      cwms_v_ts_id.ts_code%type;
+   l_crsr         sys_refcursor;
+   l_ts_ids       cwms_t_str_tab;
+   l_public_names cwms_t_str_tab;
+   l_office_ids   cwms_t_str_tab;
+   l_ts_codes     cwms_t_number_tab;
+   l_office_codes cwms_t_number_tab;
+   l_dest_dbs     cwms_t_str_tab;
+   l_dest1        cwms_t_number_tab;
+   l_dest2        cwms_t_number_tab;
+   l_seq2         pls_integer;
+
+   function in_group(p_group_id in varchar2, p_ts_id in varchar2) return boolean
    is
       l_count   pls_integer;
    begin
@@ -99,18 +104,18 @@ is
         from cwms_v_ts_grp_assgn
        where category_id = c_category_id
          and group_id = p_group_id
-         and ts_id = c_ts_id;
+         and ts_id = p_ts_id;
       return l_count != 0;
    end in_group;
 begin
-   ----------------------------
-   -- create the time series --
-   ----------------------------
-   cwms_ts.create_ts_code(
-      p_ts_code    => l_ts_code,
-      p_cwms_ts_id => c_ts_id,
-      p_office_id  => c_office_id);
    dbms_output.put_line(null);
+   l_dest1 := cwms_t_number_tab();
+   l_dest1.extend(48);
+   l_dest2 := cwms_t_number_tab();
+   l_dest2.extend(48);
+   for i in 1..l_dest2.count loop
+      l_dest2(i) := 0;
+   end loop;
    ---------------------------------------
    -- try invalid filtering combination --
    ---------------------------------------
@@ -134,6 +139,15 @@ begin
       commit;
       for j in 1..c_in_group.count loop
          l_seq := l_seq + 1;
+         ------------------------------
+         -- get the time series code --
+         ------------------------------
+         l_ts_id := c_ts_id||'-'||trim(to_char(l_seq, '09'));
+         cwms_ts.create_ts_code(
+            p_ts_code    => l_ts_code,
+            p_cwms_ts_id => l_ts_id,
+            p_office_id  => c_office_id);
+            l_ts_code := cwms_ts.get_ts_code(l_ts_id, c_office_id);
          dbms_output.put(
             to_char(l_seq, '09')||' - '
             ||case when cwms_data_dissem.is_filtering_to(c_destinations(1), c_office_id) then 'T' else 'F' end
@@ -144,29 +158,24 @@ begin
                cwms_ts.assign_ts_group(
                   p_ts_category_id => c_category_id,
                   p_ts_group_id    => c_group_ids(k),
-                  p_ts_id          => c_ts_id,
+                  p_ts_id          => l_ts_id,
                   p_db_office_id   => c_office_id);
             else
                cwms_ts.unassign_ts_group(
                   p_ts_category_id => c_category_id,
                   p_ts_group_id    => c_group_ids(k),
-                  p_ts_id          => c_ts_id,
+                  p_ts_id          => l_ts_id,
                   p_db_office_id   => c_office_id);
             end if;
             commit;
-            dbms_output.put(case when in_group(c_group_ids(k)) then 'T' else 'F' end);
+            dbms_output.put(case when in_group(c_group_ids(k), l_ts_id) then 'T' else 'F' end);
          end loop;
-         l_dest_db := null;
-         cwms_data_dissem.cat_ts_transfer(l_crsr, c_office_id);
-         fetch l_crsr into l_cwms_ts_id, l_public_name, l_office_id, l_ts_code, l_office_code, l_dest_db;
-         close l_crsr;
+         l_dest1(l_seq) := cwms_data_dissem.allowed_dest(l_ts_code);
          dbms_output.put(' ');
          dbms_output.put(case when cwms_data_dissem.allowed_to_corpsnet(l_ts_code) then 'T' else 'F' end);
          dbms_output.put(case when cwms_data_dissem.allowed_to_dmz(l_ts_code) then 'T' else 'F'end);
          dbms_output.put(' ');
-         dbms_output.put(cwms_data_dissem.allowed_dest(l_ts_code));
-         dbms_output.put(' ');
-         dbms_output.put(l_dest_db);
+         dbms_output.put(l_dest1(l_seq));
          dbms_output.put_line(null);
          case
          when not cwms_data_dissem.is_filtering_to('DMZ', c_office_id) then
@@ -177,7 +186,6 @@ begin
             ut.expect(cwms_data_dissem.allowed_to_corpsnet(l_ts_code)).to_be_true;
             ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_true;
             ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(2);
-            ut.expect(l_dest_db).to_equal(c_destinations(cwms_data_dissem.allowed_dest(l_ts_code)));
          when not cwms_data_dissem.is_filtering_to('Corpsnet', c_office_id) then
              -------------------------------
              -- all data goes to corpsnet --
@@ -186,43 +194,59 @@ begin
             ------------------------------------
             -- only filtered data goes to dmz --
             ------------------------------------
-            if in_group(c_external_include_group_id) and not in_group(c_external_exclude_group_id) and not in_group(c_internal_exclude_group_id) then
+            if in_group(c_external_include_group_id, l_ts_id) and not in_group(c_external_exclude_group_id, l_ts_id) and not in_group(c_internal_exclude_group_id, l_ts_id) then
                ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_true;
                ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(2);
             else
                ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_false;
                ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(1);
             end if;
-            ut.expect(l_dest_db).to_equal(c_destinations(cwms_data_dissem.allowed_dest(l_ts_code)));
          else
             -------------------------------------------------
             -- only filtered data goes to corpsnet and dmz --
             -------------------------------------------------
-            if in_group(c_internal_include_group_id) and not in_group(c_internal_exclude_group_id) then
+            if in_group(c_internal_include_group_id, l_ts_id) and not in_group(c_internal_exclude_group_id, l_ts_id) then
                ut.expect(cwms_data_dissem.allowed_to_corpsnet(l_ts_code)).to_be_true;
-               if in_group(c_external_include_group_id) and not in_group(c_external_exclude_group_id) then
+               if in_group(c_external_include_group_id, l_ts_id) and not in_group(c_external_exclude_group_id, l_ts_id) then
                   ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_true;
                   ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(2);
                else
                   ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_false;
                   ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(1);
                end if;
-               ut.expect(l_dest_db).to_equal(c_destinations(cwms_data_dissem.allowed_dest(l_ts_code)));
             else
-               if in_group(c_external_include_group_id) and not in_group(c_external_exclude_group_id) and not in_group(c_internal_exclude_group_id) then
+               if in_group(c_external_include_group_id, l_ts_id) and not in_group(c_external_exclude_group_id, l_ts_id) and not in_group(c_internal_exclude_group_id, l_ts_id) then
                   ut.expect(cwms_data_dissem.allowed_to_corpsnet(l_ts_code)).to_be_true;
                   ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_true;
                   ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(2);
-                  ut.expect(l_dest_db).to_equal(c_destinations(cwms_data_dissem.allowed_dest(l_ts_code)));
                else
                   ut.expect(cwms_data_dissem.allowed_to_corpsnet(l_ts_code)).to_be_false;
                   ut.expect(cwms_data_dissem.allowed_to_dmz(l_ts_code)).to_be_false;
                   ut.expect(cwms_data_dissem.allowed_dest(l_ts_code)).to_equal(0);
-                  ut.expect(l_dest_db).to_be_null;
                end if;
             end if;
          end case;
       end loop;
+      cwms_data_dissem.cat_ts_transfer(l_crsr, c_office_id);
+      fetch l_crsr
+       bulk collect
+       into l_ts_ids,
+            l_public_names,
+            l_office_ids,
+            l_ts_codes,
+            l_office_codes,
+            l_dest_dbs;
+      close l_crsr;
+      for m in 1..l_ts_ids.count loop
+         l_seq2 := to_number(cwms_util.split_text(cwms_util.split_text(l_ts_ids(m), 6, '.'), 2, '-'));
+         if l_seq2 between (i-1)*16+1 and i*16 then
+            l_dest2(l_seq2) := case when l_dest_dbs(m) = 'CorpsNet' then 1 else 2 end;
+            dbms_output.put_line(l_ts_ids(m)||chr(9)||l_dest_dbs(m));
+         end if;
+      end loop;
+   end loop;
+   for i in 1..l_dest1.count loop
+      ut.expect(l_dest2(i)).to_equal(l_dest1(i));
    end loop;
 end test_data_dissem;
 end test_cwms_data_dissem;
