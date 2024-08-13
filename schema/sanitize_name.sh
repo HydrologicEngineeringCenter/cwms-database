e8f56ee443f37e42ba73ca63e67a468e32381e3e
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-08-13 16:44:39 +0000

    Fix quoting in script.

--- schema/sanitize_name.sh
+++ schema/sanitize_name.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 INPUT=$1
-NAME=`echo $INPUT | sed -e s@/refs/heads/@@g -e s@/@_@g -e s@-@_@g -e s@#@_@g` | md5sum | sed -rn -e 's/(.*)\s+.*/\1/p'`
+NAME=`echo $INPUT | sed -e s@/refs/heads/@@g -e s@/@_@g -e s@-@_@g -e s@#@_@g | md5sum | sed -rn -e 's/(.*)\s+.*/\1/p'`
 echo "cwmsdb_$NAME"
