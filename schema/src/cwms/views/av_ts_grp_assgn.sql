f3467bec5cca6571d66bee8902605be1fb4880d5
Adam Korynta <adam@rmanet.com>
2024-11-06 11:44:06 -0800

    CWMSVUE-594 add category and group office ids to av_ts_grp_assgn

--- schema/src/cwms/views/av_ts_grp_assgn.sql
+++ schema/src/cwms/views/av_ts_grp_assgn.sql
@@ -20,7 +20,9 @@ insert into at_clob values (cwms_seq.nextval, 53, '/VIEWDOCS/AV_TS_GRP_ASSGN', n
 CREATE OR REPLACE VIEW av_ts_grp_assgn
 (
    category_id,
+   category_office_id,
    GROUP_ID,
+   group_office_id,
    ts_code,
    db_office_id,
    ts_id,
@@ -31,36 +33,34 @@ CREATE OR REPLACE VIEW av_ts_grp_assgn
    shared_ref_ts_id
 )
 AS
-   SELECT   atc.ts_category_id, atg.ts_group_id, atga.ts_code,
-            co.office_id AS db_office_id,
-            cwms_ts.get_ts_id (acts.ts_code) AS ts_id,
-            atga.ts_alias_id, atga.ts_attribute,
-            case
-              when acts2.ts_code is null then null
-              else cwms_ts.get_ts_id (acts2.ts_code)
-            end AS ref_ts_id,
-            atg.shared_ts_alias_id,
-            case
-               when acts3.ts_code is null then null
-               else cwms_ts.get_ts_id (acts3.ts_code)
-            end AS shared_ref_ts_id
-     FROM   at_cwms_ts_spec acts,
-            at_ts_group_assignment atga,
-            at_ts_group atg,
-            at_ts_category atc,
-            at_physical_location pl,
-            at_base_location bl,
-            cwms_office co,
-            at_cwms_ts_spec acts2,
-            at_cwms_ts_spec acts3
-    WHERE       pl.location_code = acts.location_code
-            AND bl.base_location_code = pl.base_location_code
-            AND co.office_code = bl.db_office_code
-            AND atga.ts_code = acts.ts_code
-            AND atga.ts_group_code = atg.ts_group_code
-            AND atg.ts_category_code = atc.ts_category_code
-            AND acts2.ts_code(+) = atga.ts_ref_code
-            AND acts3.ts_code(+) = atg.shared_ts_ref_code
+   SELECT   atc.ts_category_id,
+      atc.db_office_code as category_office_id,
+      atg.ts_group_id,
+      atg.db_office_code as group_office_id,
+      atga.ts_code,
+      co_loc.office_id AS db_office_id,
+      cwms_ts.get_ts_id (acts.ts_code) AS ts_id,
+      atga.ts_alias_id, atga.ts_attribute,
+      case
+         when acts2.ts_code is null then null
+         else cwms_ts.get_ts_id (acts2.ts_code)
+      end AS ref_ts_id,
+      atg.shared_ts_alias_id,
+      case
+         when acts3.ts_code is null then null
+         else cwms_ts.get_ts_id (acts3.ts_code)
+      end AS shared_ref_ts_id
+   FROM   at_cwms_ts_spec acts
+      INNER JOIN at_ts_group_assignment atga ON atga.ts_code = acts.ts_code
+      INNER JOIN at_ts_group atg ON atga.ts_group_code = atg.ts_group_code
+      INNER JOIN at_ts_category atc ON atg.ts_category_code = atc.ts_category_code
+      INNER JOIN at_physical_location pl ON pl.location_code = acts.location_code
+      INNER JOIN at_base_location bl ON bl.base_location_code = pl.base_location_code
+      LEFT JOIN at_cwms_ts_spec acts2 ON acts2.ts_code = atga.ts_ref_code
+      LEFT JOIN at_cwms_ts_spec acts3 ON acts3.ts_code = atg.shared_ts_ref_code
+      INNER JOIN cwms_office co_loc ON co_loc.office_code = bl.db_office_code
+      INNER JOIN cwms_office co_atc ON co_atc.office_code = atc.db_office_code
+      INNER JOIN cwms_office co_atg ON co_atg.office_code = atg.db_office_code
 /
 
 SHOW ERRORS;
