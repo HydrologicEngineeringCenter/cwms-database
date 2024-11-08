e7ae55617d7d586cb24cf2a6d70e133b6414fd7d
Mike Neilson <michael.a.neilson@usace.army.mil>
2024-11-08 00:00:43 +0000

    Move location and use dba_objects instead.

--- schema/src/buildCWMS_DB.sql
+++ schema/src/buildCWMS_DB.sql
@@ -201,10 +201,12 @@ end;
 @@cwms/User-Roles/web_user_role_grants.sql
 
 -- Grant select on tables to normal users.
+
 begin
    for rec in (select object_name
-                 from user_objects
-                where object_type = 'TABLE'
+                 from dba_objects
+                where owner = '&cwms_schema'
+                  and object_type = 'TABLE'
                   and (instr(object_name, 'AT_') = 1 or instr(object_name, 'CWMS_') = 1)
                   and instr(object_name, 'AT_SEC_') = 0
 		            and object_name != 'AT_API_KEYS'
@@ -214,10 +216,10 @@ begin
       execute immediate 'grant select on '||rec.object_name||' to cwms_user';
    end loop;
 end;
+/
 
 
-
-alter session set current_schema = &builduser
+alter session set current_schema = &builduser;
 
 -- create CWMS service user
 begin execute immediate 'create user ' || cwms_sec.cac_service_user || ' PROFILE CWMS_PROF IDENTIFIED BY "FEDCBA9876543210" '; end; 
