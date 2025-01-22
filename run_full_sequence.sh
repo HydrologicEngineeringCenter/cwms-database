6679595d3c34c092b261b32c790627b6bddac45e
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-01-22 23:11:27 +0000

    Corrections to build scripts.

--- run_full_sequence.sh
+++ run_full_sequence.sh
@@ -2,18 +2,19 @@
 BRANCH=sequence_test
 AGENT=sequence_agent
 
-ORACLE_IMAGE=gvenzl/oracle-free:23.4-full-faststart
+ORACLE_IMAGE=gvenzl/oracle-free:23.6-full-faststart
 
 cd schema
+echo "prep"
 ant docker.prepdb -Dteamcity.branch=${BRANCH}_${AGENT} -Denv.USER=$AGENT -Denv.BUILD_NUMBER=1 $*
 if [ $? -ne 0 ]; then echo "Failed to prep db"; exit 1; fi
-
+echo "schema"
 ant docker.install -Dteamcity.branch=${BRANCH}_${AGENT} -Denv.USER=$AGENT  -Denv.BUILD_NUMBER=1 $*
 if [ $? -ne 0 ]; then echo "Failed to install schema"; exit 2; fi
-
+echo "test"
 ant test -Dbuilduser.overrides=build/overrides.external.xml $*
 if [ $? -ne 0 ]; then echo "Tests failed"; exit 3; fi
-
+echo "bundle"
 mvn -Dbuilduser.overrides=build/overrides.external.xml package $*
 if [ $? -ne 0 ]; then echo "Codegen Failed"; exit 4; fi
 
