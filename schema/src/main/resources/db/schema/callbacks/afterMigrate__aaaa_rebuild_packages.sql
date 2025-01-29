50a795ce491dbb07dc484af9560229452156e9b0
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-01-29 23:32:48 +0000

    Attempt to recompile type body.

--- schema/src/main/resources/db/schema/callbacks/afterMigrate__aaaa_rebuild_packages.sql
+++ schema/src/main/resources/db/schema/callbacks/afterMigrate__aaaa_rebuild_packages.sql
@@ -1,4 +1,23 @@
 begin
    dbms_utility.compile_schema('${CWMS_SCHEMA}',false);
+   FOR cur IN (
+			   SELECT OBJECT_NAME, OBJECT_TYPE, owner, 
+			        (select count(1) from all_dependencies ad
+			         where ad.owner = ao.owner and name = OBJECT_NAME
+			        ) priority /* all_dependencies is transitive, therefore using simpler aggregate query instead of hierarchical*/
+			   FROM all_objects ao
+			   WHERE object_type in ('TYPE','TYPE BODY') and owner = '${CWMS_SCHEMA}' AND status = 'INVALID' 
+			   order by priority
+			) LOOP 
+      BEGIN
+         if cur.OBJECT_TYPE = 'TYPE BODY' then 
+            EXECUTE IMMEDIATE 'alter type ' || cur.owner || '.' || cur.OBJECT_NAME || ' compile body'; 
+         else 
+            EXECUTE IMMEDIATE 'alter ' || cur.OBJECT_TYPE || ' ' || cur.owner || '.' || cur.OBJECT_NAME || ' compile'; 
+         end if; 
+      EXCEPTION
+      WHEN OTHERS THEN NULL; 
+      END;
+   end loop;
 end;
 /
