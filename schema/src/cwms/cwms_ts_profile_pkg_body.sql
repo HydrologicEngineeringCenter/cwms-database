7550ebbb6db4ed8b5a0df9b2818183886723aeb4
Mike Perryman <32601976+perrymanmd@users.noreply.github.com>
2025-11-18 10:12:56 -0600

    Prevent validation of NULL time series IDs (#74)
    * Prevent validation of NULL time series IDs
    
    * Prevent another occurrence of NULL tsid validation

--- schema/src/cwms/cwms_ts_profile_pkg_body.sql
+++ schema/src/cwms/cwms_ts_profile_pkg_body.sql
@@ -264,7 +264,9 @@ begin
    -------------------
    if p_location_id      is null then cwms_err.raise('NULL_ARGUMENT', 'P_LOCATION_ID'  );    end if;
    if p_key_parameter_id is null then cwms_err.raise('NULL_ARGUMENT', 'P_KEY_PARAMETER_ID'); end if;
-   cwms_ts.validate_ts_id(p_ref_ts_id);
+   if p_ref_ts_id is not null then
+      cwms_ts.validate_ts_id(p_ref_ts_id);
+   end if;
    --------------------------------------
    -- populate the ts_profile_t object --
    --------------------------------------
@@ -738,7 +740,9 @@ is
    l_ts_profile  ts_profile_t;
    l_copy_parser boolean;
 begin
-   cwms_ts.validate_ts_id(p_dest_ref_ts_id);
+   if p_dest_ref_ts_id is not null then
+      cwms_ts.validate_ts_id(p_dest_ref_ts_id);
+   end if;
    l_copy_parser := cwms_util.return_true_or_false(p_copy_parser);
 
    l_ts_profile := retrieve_ts_profile_f(
