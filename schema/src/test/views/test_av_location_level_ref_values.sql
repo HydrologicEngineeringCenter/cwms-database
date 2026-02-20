41e5f7e1b5139d3d1fd094e5c7b248bcbe6d8fed
zack-rma <zack@rmanet.com>
2026-02-20 09:48:11 -0800

    Added schema to test package name

--- schema/src/test/views/test_av_location_level_ref_values.sql
+++ schema/src/test/views/test_av_location_level_ref_values.sql
@@ -5,7 +5,7 @@
  * Source may not be released without written approval from HEC
  */
 
-create or replace package test_av_location_level_ref_values as
+create or replace package &&cwms_schema..test_av_location_level_ref_values as
    --%suite(Test schema for av_location_level_ref and av_location_level_values views)
    --%rollback(manual)
    --%afterall(teardown)
@@ -138,12 +138,8 @@ create or replace package test_av_location_level_ref_values as
       </ratings>';
 end test_av_location_level_ref_values;
 /
-
 show errors;
-
-show errors
-grant execute on test_av_location_level_ref_values to cwms_user;
-create or replace package body test_av_location_level_ref_values as
+create or replace package body &&cwms_schema..test_av_location_level_ref_values as
 --------------------------------------------------------------------------------
 -- procedure teardown
 --------------------------------------------------------------------------------
