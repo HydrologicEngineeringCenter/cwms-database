71f57665093aec1dfcd7d11f25ca74991d259505
Mike Perryman <michael.d.perryman@usace.army.mil>
2026-02-26 15:25:25 +0000

    Fix another typo in update script

--- schema/src/updateScripts/update_25_7_1_to_26_2_17.sql
+++ schema/src/updateScripts/update_25_7_1_to_26_2_17.sql
@@ -76,7 +76,7 @@ whenever sqlerror continue;
 @@./26_02_17/update_cwms_nation_sp;
 @@./26_02_17/update_cwms_state;
 @@./26_02_17/update_cwms_county;
-@@./26_02_17/update_at_ts_cagegory;
+@@./26_02_17/update_at_ts_category;
 whenever sqlerror exit;
 
 PROMPT ################################################################################
