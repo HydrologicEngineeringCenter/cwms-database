bd1c257d6e440210b0c71c3dc969b80b780ab15b
mperryman2 <MPerryman@geiconsultants.com>
2026-04-29 20:29:29 +0000

    First cut at update script

---
+++ schema/src/cwms/tables/at_location_geometry.sql
@@ -0,0 +1,110 @@
+--------------------------
+-- AT_LOCATION_GEOMETRY --
+--------------------------
+create table at_location_geometry (
+   location_code number(14) not null,
+   geometry      sdo_geometry not null,
+   geometry_type number(2),
+   latitude      number,
+   longitude     number,
+   constraint at_location_geometry_pk  primary key (location_code),
+   constraint at_location_geometry_fk1 foreign key (location_code) references at_physical_location (location_code)
+);
+
+comment on table  at_location_geometry               is 'Holds geometry (point, polygon, etc...) for locations that have geometry';
+comment on column at_location_geometry.location_code is 'References location in AT_PHYSICAL_LOCATION table';
+comment on column at_location_geometry.geometry      is 'Geometry of location';
+comment on column at_location_geometry.geometry_type is 'Type of geometry (1..7 = point,line,polygon,collection,multipoint,multiline,multipolygon) - populated by trigger';
+comment on column at_location_geometry.latitude      is 'Latitude if geometry type is 1, otherwise NULL - populated by trigger';
+comment on column at_location_geometry.longitude     is 'Longitude if geometry type is 1, otherwise NULL - populated by trigger';
+
+create or replace trigger at_location_geometry_t01
+   before insert or update of geometry
+   on at_location_geometry referencing new as new old as old
+   for each row
+declare
+   l_geometry_type at_location_geometry.geometry_type%type;
+begin
+   if :new.geometry is null then
+      -------------------
+      -- null geometry --
+      -------------------
+      :new.geometry_type := null;
+      :new.latitude      := null;
+      :new.longitude     := null;
+   else
+      l_geometry_type := :new.geometry.get_gtype;
+      :new.geometry_type := l_geometry_type;
+      if l_geometry_type = 1 then
+      --------------------
+      -- point geometry --
+      --------------------
+         :new.latitude  := :new.geometry.sdo_point.y;
+         :new.longitude := :new.geometry.sdo_point.x;
+      else
+         ------------------------
+         -- non-point geometry --
+         ------------------------
+         :new.latitude  := null;
+         :new.longitude := null;
+      end if;
+   end if;
+   if :new.latitude is not null and :new.longitude is not null then
+      ---------------------------------
+      -- update AT_PHYSICAL_LOCATION --
+      ---------------------------------
+      declare
+         l_old_county_code at_physical_location.county_code%type;
+         l_old_nation_code at_physical_location.nation_code%type;
+         l_old_office_code at_physical_location.office_code%type;
+         l_old_nearest_city at_physical_location.nearest_city%type;
+         l_new_county_code at_physical_location.county_code%type;
+         l_new_nation_code at_physical_location.nation_code%type;
+         l_new_office_code at_physical_location.office_code%type;
+         l_new_nearest_city at_physical_location.nearest_city%type;
+      begin
+         select county_code,
+                nation_code,
+                office_code,
+                nearest_city
+           into l_old_county_code,
+                l_old_nation_code,
+                l_old_office_code,
+                l_old_nearest_city
+           from at_physical_location
+          where location_code = :new.location_code;
+
+         l_new_county_code  := case
+                               when l_old_county_code is null then cwms_loc.get_county_code(:new.latitude, :new.longitude)
+                               else l_old_county_code
+                               end;
+         l_new_nation_code  := case
+                               when l_old_nation_code is null then cwms_loc.get_nation_id(:new.latitude, :new.longitude)
+                               else l_old_nation_code
+                               end;
+         l_new_office_code  := case
+                               when l_old_office_code is null then cwms_loc.get_bounding_ofc_code(:new.latitude, :new.longitude)
+                               else l_old_office_code
+                               end;
+         l_new_nearest_city := case
+                               when l_old_nearest_city is null then trim(',' from trim(' ' from cwms_util.join_text(
+                                       cwms_loc.get_nearest_city(:new.latitude, :new.longitude), ', ')))
+                               else l_old_nearest_city
+                               end;
+
+         if l_new_county_code  != nvl(l_old_county_code,  -1)  or
+            l_new_nation_code  != nvl(l_old_nation_code,  '@') or
+            l_new_office_code  != nvl(l_old_office_code,  -1)  or
+            l_new_nearest_city != nvl(l_old_nearest_city, '@')
+         then
+            update at_physical_location
+               set county_code  = l_new_county_code,
+                   nation_code  = l_new_nation_code,
+                   office_code  = l_new_office_code,
+                   nearest_city = l_new_nearest_city
+             where location_code = :new.location_code;
+         end if;
+      end;
+   end if;
+end;
+/
---
+++ schema/src/updateScripts/update_latlon_to_geometry.sql
@@ -0,0 +1,197 @@
+-------------------
+-- general setup --
+-------------------
+whenever sqlerror exit;
+set define on
+set verify off
+set pages 100
+set serveroutput on
+define cwms_schema = 'CWMS_20'
+define cwms_dba_schema = 'CWMS_DBA'
+alter session set current_schema = &cwms_schema;
+------------------------------------------------------------
+-- spool to file that identifies the database in the name --
+------------------------------------------------------------
+var db_name varchar2(61)
+begin
+   select nvl(primary_db_unique_name, db_unique_name) into :db_name from v$database;
+end;
+/
+whenever sqlerror continue;
+declare
+   l_count pls_integer;
+   l_name  varchar2(30);
+begin
+   select count(*) into l_count from all_objects where object_name = 'CDB_PDBS';
+   if l_count > 0 then
+      select name
+        into l_name
+        from v$database;
+      :db_name := l_name;
+      begin
+         select pdb_name
+           into l_name
+           from cdb_pdbs;
+      exception
+         when no_data_found then
+            l_name := null;
+      end;
+      if l_name is not null then
+         :db_name := :db_name||'-'||l_name;
+      end if;
+   end if;
+end;
+/
+whenever sqlerror exit;
+column db_name new_value db_name
+select :db_name as db_name from dual;
+define logfile=update_&db_name._latlon_to_geometry.log
+PROMPT log file = &logfile
+spool &logfile append;
+-------------------
+-- do the update --
+-------------------
+PROMPT ################################################################################
+PROMPT SAVING PRE-UPDATE PRIVILEGES
+select systimestamp from dual;
+@@./util/preupdate_privs.sql;
+
+PROMPT ################################################################################
+PROMPT CREATING NEW TABLES
+select systimestamp from dual;
+
+@@../cwms/tables/at_location_geometry
+
+PROMPT ################################################################################
+PROMPT ALTERING TABLES
+select systimestamp from dual;
+
+alter table at_physical_location drop column longitude;
+alter table at_physical_location drop column latitude;
+
+@@./latlon_to_geometry/at_physical_location_t03
+
+PROMPT ################################################################################
+PROMPT CREATING AND ALTERING TYPE SPECIFICATIONS
+select systimestamp from dual;
+
+drop type location_obj_t force;
+@@../cwms_types/location_obj_t
+
+PROMPT ################################################################################
+PROMPT CREATING AND ALTERING VIEWS
+select systimestamp from dual;
+
+delete from at_clob where id in ('/VIEWDOCS/AV_LOC','/VIEWDOCS/AV_LOC2')
+@@../cwms_views/av_loc
+@@../cwms_views/av_loc2
+
+PROMPT ################################################################################
+PROMPT UPDATING PACKAGE SPECIFICATIONS
+select systimestamp from dual;
+@@../cwms_loc_pkg
+
+PROMPT ################################################################################
+PROMPT UPDATING PACKAGE BODIES
+select systimestamp from dual;
+
+@@../cwms_cat_pkg_body
+@@../cwms_embank_pkg_body
+@@../cwms_loc_pkg_body
+@@../cwms_lock_pkg_body
+@@../cwms_project_pkg_body
+
+PROMPT ################################################################################
+PROMPT FINAL HOUSEKEEPING
+select systimestamp from dual;
+
+declare
+   type usernames_t is table of varchar2(30);
+   usernames usernames_t;
+   l_count integer;
+   cmd varchar2(128);
+begin
+   select count(*) into l_count from dba_users where username='CCP';
+   usernames := usernames_t('&cwms_schema', '&cwms_dba_schema');
+   if (l_count > 0) then
+      usernames.extend;
+      usernames(usernames.count) := 'CCP';
+   end if;
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'PACKAGE BODY') loop
+      cmd := 'grant execute on &cwms_schema..'||rec.object_name||' to ';
+      dbms_output.put(cmd||'[');
+      for i in 1..usernames.count loop
+         begin
+            execute immediate(cmd||usernames(i));
+            dbms_output.put(' '||usernames(i)||'(SUCCESS)');
+         exception
+            when others then
+               dbms_output.put(' '||usernames(i)||'(FAILED)');
+         end;
+      end loop;
+      dbms_output.put_line(' ]');
+   end loop;
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'TYPE') loop
+      cmd := 'grant execute on &cwms_schema..'||rec.object_name||' to ';
+      dbms_output.put(cmd||'[');
+      for i in 1..usernames.count loop
+         begin
+            execute immediate(cmd||usernames(i));
+            dbms_output.put(' '||usernames(i)||'(SUCCESS)');
+         exception
+            when others then
+               dbms_output.put(' '||usernames(i)||'(FAILED)');
+         end;
+      end loop;
+      dbms_output.put_line(' ]');
+   end loop;
+   for rec in (select object_name from dba_objects where owner = '&cwms_schema' and object_type = 'VIEW' and object_name not like '%AQ$%') loop
+      cmd := 'grant select on &cwms_schema..'||rec.object_name||' to ';
+      dbms_output.put(cmd||'[');
+      for i in 1..usernames.count loop
+         begin
+            execute immediate(cmd||usernames(i));
+            dbms_output.put(' '||usernames(i)||'(SUCCESS)');
+         exception
+            when others then
+               dbms_output.put(' '||usernames(i)||'(FAILED)');
+         end;
+      end loop;
+      dbms_output.put_line(' ]');
+   end loop;
+end;
+/
+PROMPT ################################################################################
+PROMPT RESTORING PRE-UPDATE PRIVILEGES
+@@./util/restore_privs
+
+PROMPT ################################################################################
+PROMPT RECOMPILING SCHEMA
+select systimestamp from dual;
+@./util/compile_objects
+
+PROMPT ################################################################################
+PROMPT REMAINING INVALID OBJECTS...
+select systimestamp from dual;
+select owner||'.'||substr(object_name, 1, 30) as invalid_object,
+       object_type
+  from all_objects
+ where status = 'INVALID'
+   and owner in ('&cwms_schema', '&cwms_dba_schema')
+ order by 1, 2;
+select owner||'.'||substr(name, 1, 30) as name,
+       type,
+       substr(line||':'||position, 1, 12) as location,
+       substr(text, 1, 132) as error
+  from all_errors
+ where attribute = 'ERROR'
+   and owner in ('&cwms_schema', '&cwms_dba_schema')
+ order by owner, type, name, sequence;
+/
+
+whenever sqlerror exit;
+
+PROMPT ################################################################################
+PROMPT UPDATE COMPLETE
+select systimestamp from dual;
+exit
