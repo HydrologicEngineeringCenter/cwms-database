d9ccc8bdc92695d20ba94d44613aadcbb8456288
mperryman2 <MPerryman@geiconsultants.com>
2026-05-21 14:41:01 +0000

    Drop public synonym CWMS_SCHEDULER_AUTH

--- schema/src/updateScripts/update_issue_115-remove_scheduler_auth.sql
+++ schema/src/updateScripts/update_issue_115-remove_scheduler_auth.sql
@@ -68,6 +68,7 @@ drop view av_unauth_sched_entries;
 drop table cwms_auth_sched_entries;
 drop table cwms_unauth_sched_entries;
 
+drop public synonym cwms_scheduler_auth;
 drop public synonym cwms_v_auth_sched_entries;
 drop public synonym cwms_v_unauth_sched_entries;
 
