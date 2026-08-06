a7b832aa398de065eb442ca77cea703fd9f43fd3
zack-rma <zack@rmanet.com>
2026-08-06 10:12:44 -0700

    Initial code cleanup per feedback

--- schema/src/updateScripts/migrate_forecast_api.sql
+++ schema/src/updateScripts/migrate_forecast_api.sql
@@ -29,7 +29,6 @@ declare
    l_ts_start date;
    l_ts_end date;
    l_count number;
-   l_version_date date;
    l_distinct_count number;
    l_distinct_new_count number;
 begin
@@ -70,15 +69,14 @@ begin
       --------------------------
       -- populate new inst(s) --
       --------------------------
-      for old_ts in (select distinct forecast_date,
-                                     issue_date
+      for old_ts in (select distinct forecast_date, issue_date, version_date
                      from at_forecast_ts
                      where forecast_spec_code = old_spec.forecast_spec_code
          )
          loop
             select count(*)
             into l_distinct_count
-            from (select distinct forecast_date, issue_date
+            from (select distinct forecast_date, issue_date, version_date
                                   from at_forecast_ts
                                   where forecast_spec_code = old_spec.forecast_spec_code);
             if l_distinct_count = 1 then
@@ -94,18 +92,9 @@ begin
                into l_ts_start, l_ts_end
                from av_tsv
                where ts_code = l_ts_row.ts_code;
---                cwms_ts.CHANGE_VERSION_DATE(l_ts_row.ts_code, l_ts_row.version_date, old_ts.issue_date, l_ts_start, l_ts_end);
-               dbms_output.put_line('change version date from: ' || l_ts_row.forecast_date || ' to ' || old_ts.issue_date);
-            end if;
-            if l_distinct_count > 1 then
-               select version_date
-               into l_version_date
-               from at_forecast_ts
-               where forecast_spec_code = old_spec.forecast_spec_code
-                 and forecast_date = old_ts.forecast_date
-                 and issue_date = old_ts.issue_date
-               fetch next 1 row only;
-               l_new_inst.issue_date_time := l_version_date;
+               cwms_ts.CHANGE_VERSION_DATE(l_ts_row.ts_code, l_ts_row.version_date, old_ts.issue_date, l_ts_start, l_ts_end);
+            elsif l_distinct_count > 1 then
+               l_new_inst.issue_date_time := old_ts.version_date;
             end if;
             l_new_inst.fcst_inst_code := cwms_seq.nextval;
             l_new_inst.fcst_spec_code := l_new_spec.fcst_spec_code;
