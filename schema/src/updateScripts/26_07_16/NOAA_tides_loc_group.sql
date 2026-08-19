4d58e4c7fef27413e87f8cccd1c16a3bfd96bced
Eric Novotny <eric.v.novotny@usace.army.mil>
2026-08-19 16:50:18 -0500

    Add noaa tides loc group (#219)
    * add noaa tides and currents to loc group
    
    * add update file
    
    ---------
    
    Co-authored-by: Novotny <1533907136121002@mil>

---
+++ schema/src/updateScripts/26_07_16/NOAA_tides_loc_group.sql
@@ -0,0 +1,23 @@
+insert into at_loc_group (
+   loc_group_code,
+   loc_category_code,
+   loc_group_id,
+   loc_group_desc,
+   db_office_code,
+   shared_loc_alias_id,
+   shared_loc_ref_code,
+   loc_group_attribute
+)
+   select 12,
+          1,
+          'NOAA Tides and Currents ID',
+          'NOAA Tides and Currents Station IDs',
+          53,
+          null,
+          null null
+     from dual
+    where not exists (
+      select 1
+        from at_loc_group
+       where loc_group_code = 12
+   );
