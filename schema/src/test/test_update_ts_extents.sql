2ff151d1913b16f240cac7d8c0c953f7e1484e93
mperryman2 <MPerryman@geiconsultants.com>
2026-09-24 20:51:38 +0000

    Increase wait for UTX job in test

--- schema/src/test/test_update_ts_extents.sql
+++ schema/src/test/test_update_ts_extents.sql
@@ -1269,7 +1269,7 @@ begin
       p_office_id       => c_office_id);
    commit;
 
-   dbms_session.sleep(8); -- wait for utx job to finish
+   dbms_session.sleep(15); -- wait for utx job to finish
 
    l_ts_extents := cwms_ts.get_ts_extents_f(
       p_cwms_ts_id   => c_ts_id,
